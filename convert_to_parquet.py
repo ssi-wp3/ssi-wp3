cf6ecfb31d0fa771b30dfdc2ec4d51fc56985c5e
Tim De Jong <gitlab@thinkpractice.nl>
2023-10-09 15:10:09 +0200

    Adapted convert_to_parquet.py to read xslx files instead and use environment variables.

--- convert_to_parquet.py
+++ convert_to_parquet.py
@@ -1,10 +1,11 @@
+from dotenv import load_dotenv
 import argparse
 import pandas as pd
 import os
 import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input-directory", required=True,
+parser.add_argument("-i", "--input-directory",
                     help="The directory to read the csv files from")
 parser.add_argument("-o", "--output-directory", default=None,
                     help="The directory to read the parquet files to")
@@ -12,13 +13,23 @@ parser.add_argument("-e", "--extension", default=".xlsx",
                     help="The extension for the xlsx files")
 args = parser.parse_args()
 
-input_filenames = [os.path.join(args.input_directory, filename)
-                   for filename in os.listdir(args.input_directory)
+load_dotenv()
+
+input_directory = os.getenv(
+    "INPUT_DIRECTORY") if not args.input_directory else args.input_directory
+
+# If no output directory is specified, try to get output directory from env
+# If no output directory is specified in env, use input directory.
+output_directory = os.getenv(
+    "OUTPUT_DIRECTORY") if not args.output_directory else args.output_directory
+output_directory = input_directory if not output_directory else output_directory
+
+input_filenames = [os.path.join(input_directory, filename)
+                   for filename in os.listdir(input_directory)
                    if filename.endswith(args.extension)]
-output_directory = args.input_directory if not args.output_directory else args.output_directory
 
 print(
-    f"Reading all {args.extension} files from {args.input_directory} and writing them to {output_directory}")
+    f"Reading all {args.extension} files from {input_directory} and writing them to {output_directory}")
 progress_bar = tqdm.tqdm(input_filenames)
 for input_filename in progress_bar:
     filename = os.path.basename(input_filename).replace(args.extension, "")
