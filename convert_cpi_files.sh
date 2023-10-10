f5cb56c042e857f1fc7d2c5d3dd7cbf3d74105ef
Tim De Jong <gitlab@thinkpractice.nl>
2023-10-10 11:03:23 +0200

    Added bash script to convert CPI files.

---
+++ convert_cpi_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# The CPI files use a weird encoding, convert the files first to utf-8 and convert them to parquet later
+# Read all files in the input directory, skip the first 3 bytes (BOM), convert them from cp1252 to utf-8
+# and filter out the carriage returns '\r' and write the result to the output directory
+for file in $input_directory/*.csv do
+    tail -c +4 $file | iconv -f cp1252 -t utf-8 | tr -d '\r' > $output_directory/$(basename $file)
+done
