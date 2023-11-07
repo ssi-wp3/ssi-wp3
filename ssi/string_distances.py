9492f0885ad04249a4169d7980767254cc568f4b
Tim De Jong <gitlab@thinkpractice.nl>
2023-11-07 10:19:50 +0100

    Some refactoring.

--- ssi/string_distances.py
+++ ssi/string_distances.py
@@ -1,23 +1,32 @@
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 
-def tfidf_similarity(dataframe: pd.DataFrame, stop_words: str = 'dutch') -> pd.DataFrame:
-    # Assuming df is your DataFrame, 'text' is the column with the receipt texts, and 'coicop' is the COICOP category
-    vectorizer = TfidfVectorizer(stop_words=stop_words)
-    X = vectorizer.fit_transform(dataframe['text'])
-    return cosine_similarity(X)
-  
-def tfidf_similarity_per_group(dataframe: pd.DataFrame, stop_words: str = 'dutch') -> pd.DataFrame:
-    similarity_matrix = tfidf_similarity(dataframe, stop_words)
+def vector_similarity_per_group(dataframe: pd.DataFrame, features: np.array) -> pd.DataFrame:
+    similarity_matrix = cosine_similarity(features)
     dataframe['similarity'] = similarity_matrix.mean(axis=1)
     return dataframe.groupby('coicop')['similarity'].agg(['mean', 'std'])
 
-def similary_plot(df: pd.DataFrame, stop_words: str = 'dutch'):
+def tfidf_features(dataframe, stop_words, text_column):
+    vectorizer = TfidfVectorizer(stop_words=stop_words)
+    return vectorizer.fit_transform(dataframe[text_column])
+
+def tfidf_similarity(dataframe: pd.DataFrame, stop_words: str = 'dutch', text_column: str = "ean_name") -> pd.DataFrame:
+    # Assuming df is your DataFrame, 'text' is the column with the receipt texts, and 'coicop' is the COICOP category
+    X = tfidf_features(dataframe, stop_words, text_column)
+    return cosine_similarity(X)
+  
+def tfidf_similarity_per_group(dataframe: pd.DataFrame, stop_words: str = 'dutch') -> pd.DataFrame:
+    X = tfidf_features(dataframe, stop_words) 
+    return vector_similarity_per_group(dataframe, X)
+
+def similary_plot(df: pd.DataFrame, stop_words: str = 'dutch', text_column: str = "ean_name"):
     # Assuming similarity_df is the DataFrame returned by the tfidf_similarity function
-    similarity_df = tfidf_similarity(df, stop_words)
+    similarity_df = tfidf_similarity(df, stop_words, text_column)
 
     similarity_df.plot(kind='bar', y='mean', yerr='std', capsize=4)
     plt.title('Similarity Scores by COICOP Category')
@@ -25,13 +34,20 @@ def similary_plot(df: pd.DataFrame, stop_words: str = 'dutch'):
     plt.ylabel('Similarity Score')
     plt.show()
 
-
-def count_vectorizer_similarity(dataframe: pd.DataFrame, stop_words: str = 'dutch') -> pd.DataFrame:
+def count_vectorizer_features(dataframe, stop_words, text_column):
     vectorizer = CountVectorizer(stop_words=stop_words)
-    X = vectorizer.fit_transform(dataframe['text'])
+    return vectorizer.fit_transform(dataframe[text_column])
+
+def count_vectorizer_similarity(dataframe: pd.DataFrame, stop_words: str = 'dutch', text_column: str = 'ean_name') -> pd.DataFrame:
+    X = count_vectorizer_features(dataframe, stop_words, text_column)
     return cosine_similarity(X)
 
-def count_vectorizer_similarity_per_group(dataframe: pd.DataFrame, stop_words: str = 'dutch') -> pd.DataFrame:
-    similarity_matrix = count_vectorizer_similarity(dataframe, stop_words)
-    dataframe['similarity'] = similarity_matrix.mean(axis=1)
-    return dataframe.groupby('coicop')['similarity'].agg(['mean', 'std'])
+def count_vectorizer_similarity_per_group(dataframe: pd.DataFrame, stop_words: str = 'dutch', text_column: str = 'ean_name') -> pd.DataFrame:
+    X = count_vectorizer_features(dataframe, stop_words, text_column)
+    return vector_similarity_per_group(dataframe, X) 
+
+def plot_similarity_heatmap(similarity_matrix: pd.DataFrame):
+    # Assuming similarity_matrix is the cosine similarity matrix
+    sns.heatmap(similarity_matrix, cmap='coolwarm')
+    plt.title('Similarity Matrix')
+    plt.show()
