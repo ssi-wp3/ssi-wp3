354705e06e2d2605cf7d046f0ec0389622b779da
Tim De Jong <gitlab@thinkpractice.nl>
2023-12-05 10:18:54 +0100

    Added tqdm progress bar.

--- ssi/data_exploration.py
+++ ssi/data_exploration.py
@@ -1,14 +1,19 @@
 from typing import List
 import pandas as pd
 import os
+import tqdm
 
 def filter_coicop_level(dataframe: pd.DataFrame, coicop_level_column: str, coicop_level_value: str) -> pd.DataFrame:
     return dataframe[dataframe[coicop_level_column] == coicop_level_value]
 
 def write_filtered_coicop_level_files(dataframe: pd.DataFrame, coicop_level_columns: List[str], data_directory: str, supermarket_name: str):
-    for coicop_level_column in coicop_level_columns:    
-        for coicop_level_value in dataframe[coicop_level_column].unique():
-            filtered_dataframe = filter_coicop_level(
-                dataframe, coicop_level_column, coicop_level_value)
-            filtered_dataframe.to_parquet(
-                os.path.join(data_directory, f"{supermarket_name}_{coicop_level_value}.parquet"), engine="pyarrow", index=False)
+    number_of_files = len(coicop_level_columns) * len(dataframe[coicop_level_columns[0]].unique())
+    with tqdm.tqdm(total=number_of_files) as progress_bar:
+        for coicop_level_column in coicop_level_columns:    
+            for coicop_level_value in dataframe[coicop_level_column].unique():
+                progress_bar.set_description(f"Writing {supermarket_name}_{coicop_level_value}.parquet")
+                filtered_dataframe = filter_coicop_level(
+                    dataframe, coicop_level_column, coicop_level_value)
+                filtered_dataframe.to_parquet(
+                    os.path.join(data_directory, f"{supermarket_name}_{coicop_level_value}.parquet"), engine="pyarrow", index=False)
+                progress_bar.update(1)    
