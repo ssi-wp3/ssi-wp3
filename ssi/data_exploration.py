d483a7b1ea9f9cfc3cb3f4ea5330737a9c1884d5
Tim De Jong <gitlab@thinkpractice.nl>
2023-12-07 11:36:47 +0100

    More work on ProductAnalysis.

--- ssi/data_exploration.py
+++ ssi/data_exploration.py
@@ -3,6 +3,7 @@ from .plots import sunburst_coicop_levels
 import pandas as pd
 import os
 import tqdm
+import wordcloud
 
 
 def filter_coicop_level(dataframe: pd.DataFrame, coicop_level_column: str, coicop_level_value: str) -> pd.DataFrame:
@@ -53,5 +54,27 @@ class ProductAnalysis:
         sunburst_coicop_levels(
             dataframe, self.coicop_level_columns, self.plot_directory, sunburst_filename)
 
+    def plot_wordcloud(self, dataframe: pd.DataFrame, product_description_column: str, filename: str):
+        wordcloud = wordcloud.WordCloud()
+        product_descriptions = " ".join(
+            dataframe[product_description_column].tolist())
+        wordcloud.generate_from_text(product_descriptions).to_file(filename)
+
+    def perform_product_analysis_per_coicop_level(self, dataframe: pd.DataFrame, coicop_level: str, product_description_column: str = "ean_name"):
+        coicop_level_values = dataframe[coicop_level].unique()
+        for coicop_level_value in coicop_level_values:
+            coicop_level_value_df = filter_coicop_level(
+                dataframe, coicop_level, coicop_level_value)
+            wordcloud_filename = os.path.join(
+                self.plot_directory, f"products_{self.supermarket_name}_{coicop_level}_{coicop_level_value}_wordcloud.png")
+            self.plot_wordcloud(coicop_level_value_df,
+                                product_description_column, wordcloud_filename)
+
+    def perform_product_level_analysis(self, dataframe: pd.DataFrame):
+        for coicop_level in self.coicop_level_columns:
+            self.perform_product_analysis_per_coicop_level(
+                dataframe, coicop_level)
+
     def analyze_products(self, dataframe: pd.DataFrame):
         self.plot_sunburst(dataframe)
+        self.perform_product_level_analysis(dataframe)
