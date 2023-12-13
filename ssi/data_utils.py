f1f5400b033da9b7db9b0a5c6d8824492bccd1cd
Tim De Jong <gitlab@thinkpractice.nl>
2023-12-13 16:55:22 +0100

    Only log if directory does not exist

--- ssi/data_utils.py
+++ ssi/data_utils.py
@@ -5,12 +5,14 @@ import os
 def export_dataframe(dataframe: pd.DataFrame, path: str, filename: str, index: bool = False, delimiter: str = ";"):
     csv_directory = os.path.join(path, "csv")
 
-    print(f"Creating log directory {csv_directory}")
-    os.makedirs(csv_directory, exist_ok=True)
+    if not os.path.exists(csv_directory):
+        print(f"Creating log directory {csv_directory}")
+        os.makedirs(csv_directory, exist_ok=True)
 
     html_directory = os.path.join(path, "html")
-    print(f"Creating log directory {html_directory}")
-    os.makedirs(html_directory, exist_ok=True)
+    if not os.path.exists(html_directory):
+        print(f"Creating log directory {html_directory}")
+        os.makedirs(html_directory, exist_ok=True)
 
     dataframe.to_csv(os.path.join(
         csv_directory, filename + ".csv"), sep=delimiter, index=index)
