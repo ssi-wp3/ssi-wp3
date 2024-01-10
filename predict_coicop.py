a6f028b11cb2edf51ce44e1222ba9480c76afe75
Tim De Jong <gitlab@thinkpractice.nl>
2024-01-10 19:07:22 +0100

    Example of a script predicting coicop labels for a range of input strings.

---
+++ predict_coicop.py
@@ -0,0 +1,46 @@
+from typing import Any, Dict, List
+import argparse
+import os
+import joblib
+import numpy as np
+
+# Adapted from https://huggingface.co/scikit-learn/sklearn-transformers/blob/main/pipeline.py
+
+
+class CoicopPipeline():
+    def __init__(self, pipeline_path: str):
+        # load the model
+        self.model = joblib.load(pipeline_path)
+
+    def __call__(self, inputs: List[str]):
+        return self.predict_proba(inputs)
+
+    def predict(self, inputs: List[str]) -> List[str]:
+        return self.model.predict(inputs)
+
+    def predict_proba(self, inputs: List[str]) -> List[Dict[str, Any]]:
+        predictions = self.model.predict_proba(inputs)
+        prediction_labels = []
+        for prediction in predictions:
+            label_predictions = dict()
+            for index, probability in enumerate(prediction):
+                label = self.model.classes_[index]
+                label_predictions[label] = probability,
+            prediction_labels.append(label_predictions)
+        return prediction_labels
+
+
+def main(args):
+    pipeline = CoicopPipeline(args.pipeline_path)
+    predictions = pipeline.predict_proba(args.inputs)
+    print(predictions)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Predict COICOP')
+    parser.add_argument("-p", "--pipeline-path", type=str,
+                        default="pipeline.joblib", help="Path to pipeline")
+    parser.add_argument("-i", "--inputs", type=str, nargs="+",
+                        default=[], help="Inputs to predict")
+    args = parser.parse_args()
+    main(args)
