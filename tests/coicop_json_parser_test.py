b68b8b89935daea33a98fdd1c766ebb8532b29c1
Tim De Jong <gitlab@thinkpractice.nl>
2024-01-15 11:20:48 +0100

    Added test for ClassificationResult.

--- tests/coicop_json_parser_test.py
+++ tests/coicop_json_parser_test.py
@@ -194,3 +194,153 @@ class CoicopJsonParserTest(unittest.TestCase):
         }
         coicop_input_file = CoicopInputFile.model_validate(json)
         self.assertEqual(json, coicop_input_file.model_dump())
+
+    def test_coicop_classification_serialized_to_json(self):
+        expected_json = {
+            "code": "1",
+            "description": "item1",
+            "confidence": 0.5
+        }
+
+        coicop_classification = CoicopClassification(
+            code="1",
+            description="item1",
+            confidence=0.5
+        )
+        self.assertEqual(expected_json, coicop_classification.model_dump())
+
+    def test_coicop_classification_read_from_json(self):
+        json = {
+            "code": "1",
+            "description": "item1",
+            "confidence": 0.5
+        }
+        coicop_classification = CoicopClassification.model_validate(json)
+        self.assertEqual(json, coicop_classification.model_dump())
+
+    def test_product_classification_result_serialized_to_json(self):
+        expected_json = {
+            "id": "1",
+            "coicop_codes": [
+                {
+                    "code": "1",
+                    "description": "coicop1",
+                    "confidence": 0.75
+                },
+                {
+                    "code": "2",
+                    "description": "coicop2",
+                    "confidence": 0.85
+                }
+            ]
+        }
+
+        product_classification_result = ProductClassificationResult(
+            id="1",
+            coicop_codes=[
+                CoicopClassification(
+                    code="1",
+                    description="coicop1",
+                    confidence=0.75
+                ),
+                CoicopClassification(
+                    code="2",
+                    description="coicop2",
+                    confidence=0.85
+                )
+            ]
+        )
+        self.assertEqual(
+            expected_json, product_classification_result.model_dump())
+
+    def test_product_classification_result_read_from_json(self):
+        json = {
+            "id": "1",
+            "coicop_codes": [
+                {
+                    "code": "1",
+                    "description": "coicop1",
+                    "confidence": 0.75
+                },
+                {
+                    "code": "2",
+                    "description": "coicop2",
+                    "confidence": 0.85
+                }
+            ]
+        }
+        product_classification_result = ProductClassificationResult.model_validate(
+            json)
+        self.assertEqual(json, product_classification_result.model_dump())
+
+    def test_classification_result_serialized_to_json(self):
+        expected_json = {
+            "result": [
+                {
+                    "id": "1",
+                    "coicop_codes": [
+                        {
+                            "code": "1",
+                            "description": "coicop1",
+                            "confidence": 0.75
+                        },
+                        {
+                            "code": "2",
+                            "description": "coicop2",
+                            "confidence": 0.85
+                        }
+                    ]
+                },
+                {
+                    "id": "2",
+                    "coicop_codes": [
+                        {
+                            "code": "3",
+                            "description": "coicop3",
+                            "confidence": 0.65
+                        },
+                        {
+                            "code": "4",
+                            "description": "coicop4",
+                            "confidence": 0.95
+                        }
+                    ]
+                }
+            ]
+        }
+
+        classification_result = ClassificationResult(
+            result=[
+                ProductClassificationResult(
+                    id="1",
+                    coicop_codes=[
+                        CoicopClassification(
+                            code="1",
+                            description="coicop1",
+                            confidence=0.75
+                        ),
+                        CoicopClassification(
+                            code="2",
+                            description="coicop2",
+                            confidence=0.85
+                        )
+                    ]
+                ),
+                ProductClassificationResult(
+                    id="2",
+                    coicop_codes=[
+                        CoicopClassification(
+                            code="3",
+                            description="coicop3",
+                            confidence=0.65
+                        ),
+                        CoicopClassification(
+                            code="4",
+                            description="coicop4",
+                            confidence=0.95
+                        )
+                    ]
+                )
+            ]
+        )
+        self.assertEqual(expected_json, classification_result.model_dump())
