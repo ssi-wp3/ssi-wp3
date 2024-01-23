982956b80e376ed4d20646c5a4a7528abfb79324
Tim De Jong <gitlab@thinkpractice.nl>
2024-01-23 17:04:50 +0100

    Added a predict script that reads a parquet file and predicts on the basis of the receipt_text. After that it writes the predictions to another parquet file.

--- ssi/predict.py
+++ ssi/predict.py
@@ -4,3 +4,9 @@ def predict(pipeline, dataframe: pd.DataFrame, label_column: str, column_prefix:
     y_true = dataframe[label_column]
     dataframe[f"{column_prefix}{label_column}"] = pipeline.predict(y_true)
     return dataframe
+
+
+def predict_from_file(pipeline, input_filename: str, output_filename: str, label_column: str, column_prefix: str = "predict_"):
+    dataframe = pd.read_parquet(input_filename, engine="pyarrow")
+    dataframe = predict(pipeline, dataframe, label_column, column_prefix)
+    dataframe.to_parquet(output_filename, engine="pyarrow")
