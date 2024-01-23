fb894da35c08f893578a8cc62e326342a2deb9dc
Tim De Jong <gitlab@thinkpractice.nl>
2024-01-23 16:32:38 +0100

    Some refactoring.

--- predict_coicop.py
+++ predict_coicop.py
@@ -1,42 +1,8 @@
-from ssi.coicop_json_parser import CoicopInputFile, CoicopOutputFile, load_input_file, create_coicop_output_file
-from typing import Any, Dict, List
+from ssi.coicop_json_parser import load_input_file
+from ssi.coicop_pipeline import CoicopPipeline
 import argparse
-import joblib
 import json
 # Adapted from https://huggingface.co/scikit-learn/sklearn-transformers/blob/main/pipeline.py
-
-
-class CoicopPipeline():
-    def __init__(self, pipeline_path: str):
-        # load the model
-        self.model = joblib.load(pipeline_path)
-
-    def __call__(self, inputs: List[str]):
-        return self.predict_proba(inputs)
-
-    def predict(self, inputs: List[str]) -> List[str]:
-        return self.model.predict(inputs)
-
-    def predict_proba(self, inputs: List[str]) -> List[Dict[str, Any]]:
-        predictions = self.model.predict_proba(inputs)
-        prediction_labels = []
-        for prediction in predictions:
-            label_predictions = dict()
-            for index, probability in enumerate(prediction):
-                label = self.model.classes_[index]
-                label_predictions[label] = probability
-            prediction_labels.append(label_predictions)
-        return prediction_labels
-
-    def predict_receipt(self, receipt_input: CoicopInputFile) -> CoicopOutputFile:
-        receipt_ids = [item.id for item in receipt_input.receipt.items]
-        receipt_descriptions = [item.description
-                                for item in receipt_input.receipt.items]
-        predicted_probabilities = self.predict_proba(receipt_descriptions)
-        
-        return create_coicop_output_file(receipt_input, receipt_ids, predicted_probabilities)
-
-
 
 def main(args):
     try:
