097b8c0899f35bcbb0d5908f2b4e7463c6e595b7
Tim De Jong <gitlab@thinkpractice.nl>
2024-02-23 18:02:01 +0100

    Small change

--- ssi/preprocessing/files.py
+++ ssi/preprocessing/files.py
@@ -3,5 +3,6 @@ import os
 
 
 def get_revenue_files_in_folder(data_directory: str, supermarket_name: str, filename_prefix: str = "Omzet") -> List[str]:
-    return [os.path.join(data_directory, filename) for filename in os.listdir(
-        data_directory) if filename.startswith(filename_prefix) and supermarket_name in filename]
+    return [os.path.join(data_directory, filename)
+            for filename in os.listdir(data_directory)
+            if filename.startswith(filename_prefix) and supermarket_name in filename]
