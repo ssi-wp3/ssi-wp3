472a45a36691dcbcd51393b94c83d2fda866be25
Tim De Jong <gitlab@thinkpractice.nl>
2024-02-23 18:02:58 +0100

    Made it possible to filter files on file_type.

--- ssi/preprocessing/files.py
+++ ssi/preprocessing/files.py
@@ -1,8 +1,8 @@
-from typing import List
+from typing import List, Optional
 import os
 
 
-def get_revenue_files_in_folder(data_directory: str, supermarket_name: str, filename_prefix: str = "Omzet") -> List[str]:
+def get_revenue_files_in_folder(data_directory: str, supermarket_name: str, filename_prefix: str = "Omzet", file_type: Optional[str] = None) -> List[str]:
     return [os.path.join(data_directory, filename)
             for filename in os.listdir(data_directory)
-            if filename.startswith(filename_prefix) and supermarket_name in filename]
+            if filename.startswith(filename_prefix) and supermarket_name in filename and (not file_type or filename.endswith(file_type))]
