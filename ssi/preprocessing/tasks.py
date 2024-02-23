60fcae08ac947912560bfd65f310c695f00b078a
Tim De Jong <gitlab@thinkpractice.nl>
2024-02-23 18:09:19 +0100

    Added first version of PreprocessCombinedFile. Needs some work still though.

--- ssi/preprocessing/tasks.py
+++ ssi/preprocessing/tasks.py
@@ -1,6 +1,7 @@
 from luigi.contrib.external_program import ExternalProgramTask
 from .parquet import convert_to_parquet
 from .files import get_revenue_files_in_folder
+from .preprocess_data import preprocess_data
 import pandas as pd
 import pathlib
 import luigi
@@ -135,3 +136,74 @@ class CombineRevenueFiles(luigi.Task):
         with self.output().open('w') as output_file:
             combined_dataframe.to_parquet(
                 output_file, engine=self.parquet_engine)
+
+
+class PreprocessCombinedFile(luigi.Task):
+    """ This task preprocesses the combined revenue file.
+
+    Parameters
+    ----------
+    input_filename : luigi.Parameter
+        The input filename of the combined revenue file.
+
+    output_filename : luigi.Parameter
+        The output filename for the preprocessed combined revenue file.
+
+    coicop_column : luigi.Parameter
+        The name of the column containing the coicop numbers.
+
+    product_id_column : luigi.Parameter
+        The name of the column containing the product ids.
+
+    product_description_column : luigi.Parameter
+        The name of the column containing the product descriptions.
+
+    selected_columns : luigi.ListParameter
+        Names of the columns to select.
+
+    coicop_level_columns : luigi.ListParameter
+        Names of the columns containing the coicop levels.
+
+    column_mapping : luigi.DictParameter
+        A dictionary containing the mapping of column names.
+
+    """
+    input_filename = luigi.Parameter()
+    output_filename = luigi.Parameter()
+    coicop_column = luigi.Parameter(default="coicop_number")
+    product_id_column = luigi.Parameter(default="product_id")
+    product_description_column = luigi.Parameter(default="ean_name")
+    selected_columns = luigi.ListParameter(default=[])
+    coicop_level_columns = luigi.ListParameter(default=[])
+    column_mapping = luigi.DictParameter(default={})
+    parquet_engine = luigi.Parameter(default="pyarrow")
+
+    def requires(self):
+        # TODO: check input_directory is derived wrongly, unless the combined file is in the same directory as the separate revenue files
+        return CombineRevenueFiles(input_directory=os.path.dirname(self.input_filename),
+                                   output_filename=self.input_filename.replace(
+                                       ".parquet", "_preprocessed.parquet"),
+                                   store_name=self.store_name,
+                                   filename_prefix=self.filename_prefix,
+                                   parquet_engine=self.parquet_engine,
+                                   sort_order=self.sort_order)
+
+    def run(self):
+        with self.input().open("r") as input_file:
+            combined_df = pd.read_parquet(
+                input_file, engine=self.parquet_engine)
+
+        combined_df = preprocess_data(
+            combined_df,
+            columns=self.selected_columns,
+            coicop_column=self.coicop_column,
+            product_id_column=self.product_id_column,
+            product_description_column=self.product_description_column,
+            column_mapping=self.column_mapping
+        )
+
+        with self.output().open('w') as output_file:
+            combined_df.to_parquet(output_file, engine=self.parquet_engine)
+
+    def output(self):
+        return luigi.LocalTarget(self.output_filename, format=luigi.format.Nop)
