6b811ed1960475c3c57fc31daddc2446156efbb5
Tim De Jong <gitlab@thinkpractice.nl>
2024-02-23 16:02:54 +0100

    A bit more work on the ConvertCPIFiles task, now it only converts a single file and we renamed it to CleanCPIFile.

--- ssi/preprocessing/tasks.py
+++ ssi/preprocessing/tasks.py
@@ -2,9 +2,25 @@ from luigi.contrib.external_program import ExternalProgramTask
 import luigi
 
 
-class ConvertCPIFiles(ExternalProgramTask):
-    input_directory = luigi.PathParameter()
-    output_directory = luigi.PathParameter()
+class CleanCPIFile(ExternalProgramTask):
+    """ This task converts CBS CPI CSV files to CSV file format that can be read by python.
+
+    The CBS CPI files use an unusual encoding. The shell script takes an input 
+    file, reads the input_filename, skips the first 3 bytes (BOM), and converts 
+    them from cp1252 to utf-8. Last, it filters out the carriage returns '\r' 
+    and writes the result to a file with output_filename.
+
+    Parameters
+    ----------
+    input_filename : luigi.PathParameter
+        The input filename of the original CBS CPI file.
+
+    output_filename : luigi.PathParameter
+        The output filename for the cleaned CBS CPI file.
+
+    """
+    input_filename = luigi.PathParameter()
+    output_filename = luigi.PathParameter()
 
     def program_args(self):
-        return ['bash', './convert_cpi_files.sh', self.input_directory, self.output_directory]
+        return ['bash', './convert_cpi_file.sh', self.input_directory, self.output_directory]
