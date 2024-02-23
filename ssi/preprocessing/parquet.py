e7da82f00e421fc4f16bd03410fc8c18e9a2d55f
Tim De Jong <gitlab@thinkpractice.nl>
2024-02-23 16:18:57 +0100

    Created a task that takes a  cleaned csv and converts it to parquet.

---
+++ ssi/preprocessing/parquet.py
@@ -0,0 +1,69 @@
+from typing import Dict, Any, Optional
+from collections import OrderedDict
+import argparse
+import pandas as pd
+import os
+import tqdm
+import numpy as np
+
+
+def get_column_types(filename: str) -> Optional[OrderedDict[str, Any]]:
+    # From Justin's code, but converted them to english and lower case
+    column_types = OrderedDict([
+        ('bg_number', str),
+        ('month', str),
+        ('coicop_number', str),
+        ('coicop_name', str),
+        ('isba_number', str),
+        ('isba_name', str),
+        ('esba_number', str),
+        ('esba_name', str),
+        ('rep_id', str),
+        ('ean_number', str),
+        ('ean_name', str),
+        ('revenue', np.float32),
+        ('amount', np.float32)
+    ])
+
+    if filename.lower().startswith("omzeteans"):
+        return column_types
+    elif filename.lower().startswith("output"):
+        return OrderedDict([(column_name, column_types[column_name])
+                            for column_name in ['bg_number', 'coicop_number', 'coicop_name', 'isba_number', 'isba_name', 'esba_number', 'esba_name', 'rep_id', 'ean_number', 'ean_name']
+                            ])
+    return None
+
+
+def get_columns_to_rename(filename: str) -> Optional[Dict[str, str]]:
+    if filename.lower().startswith("kassabon"):
+        return {
+            'Datum_vanaf': 'start_date',
+            'Ean': 'ean_number',
+            'Kassabon': 'receipt_text',
+            'RPK_REP_id': 'rep_id'
+        }
+    return None
+
+
+def convert_to_parquet(input_filename: str,
+                       input_file,
+                       output_file,
+                       delimiter: str = ";",
+                       encoding: str = "utf-8",
+                       extension: str = ".csv",
+                       decimal: str = ",") -> None:
+
+    filename = os.path.basename(input_filename).replace(extension, "")
+
+    # Add header names and types to all but kassa files
+    header_types = get_column_types(filename)
+    header_names = None if not header_types else [
+        name for name in header_types.keys()]
+    df = pd.read_csv(input_file, sep=delimiter, engine="pyarrow",
+                     encoding=encoding, decimal=decimal,
+                     names=header_names, dtype=header_types, parse_dates=True)
+
+    columns_to_rename = get_columns_to_rename(filename)
+    if columns_to_rename:
+        df = df.rename(columns=columns_to_rename)
+    df.to_parquet(output_file, engine="pyarrow")
--- ssi/preprocessing/tasks.py
+++ ssi/preprocessing/tasks.py
@@ -1,4 +1,6 @@
 from luigi.contrib.external_program import ExternalProgramTask
+from parquet import convert_to_parquet
+import pandas as pd
 import luigi
 
 
@@ -24,3 +26,51 @@ class CleanCPIFile(ExternalProgramTask):
 
     def program_args(self):
         return ['bash', './convert_cpi_file.sh', self.input_directory, self.output_directory]
+
+    def output(self):
+        """
+
+        TODO: check if this doesn't cause the atomic writes problem mentioned here: 
+        https://luigi.readthedocs.io/en/latest/luigi_patterns.html#atomic-writes-problem
+        """
+        return luigi.LocalTarget(self.output_filename)
+
+
+class ConvertCSVToParquet(luigi.Task):
+    """ This task converts a CSV file to a parquet file.
+
+    Parameters
+    ----------
+    input_filename : luigi.PathParameter
+        The input filename of the original CSV file.
+
+    output_filename : luigi.PathParameter
+        The output filename for the parquet file.
+
+    delimiter : luigi.Parameter
+        The delimiter used in the CSV file.
+
+    """
+    input_filename = luigi.PathParameter()
+    output_filename = luigi.PathParameter()
+    delimiter = luigi.Parameter(default=';')
+    encoding = luigi.Parameter(default='utf-8')
+    extension = luigi.Parameter(default='.csv')
+    decimal = luigi.Parameter(default=',')
+
+    def requires(self):
+        return CleanCPIFile(input_filename=self.input_filename, output_filename=self.output_filename)
+
+    def run(self):
+        with self.input().open('r') as input_file:
+            with self.output().open('w') as output_file:
+                convert_to_parquet(self.input_filename,
+                                   input_file,
+                                   output_file,
+                                   delimiter=self.delimiter,
+                                   encoding=self.encoding,
+                                   extension=self.extension,
+                                   decimal=',')
+
+    def output(self):
+        return luigi.LocalTarget(self.output_filename)
