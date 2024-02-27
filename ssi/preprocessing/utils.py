36c7f670ec78d62173365d1fd2e4933af0e4b435
Tim De Jong <gitlab@thinkpractice.nl>
2024-02-27 14:43:25 +0100

    ParquetFile should be opened in binary mode.

--- ssi/preprocessing/utils.py
+++ ssi/preprocessing/utils.py
@@ -6,4 +6,4 @@ class ParquetFile(luigi.ExternalTask):
     input_filename = luigi.Parameter()
 
     def output(self):
-        return luigi.LocalTarget(self.input_filename)
+        return luigi.LocalTarget(self.input_filename, format=luigi.format.Nop)
