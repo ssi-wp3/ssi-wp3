cf562b199866f3fa43f59a815d85545e6918a0a7
Tim De Jong <gitlab@thinkpractice.nl>
2024-02-27 10:37:48 +0100

    Small change

--- ssi/analysis/tasks.py
+++ ssi/analysis/tasks.py
@@ -4,12 +4,20 @@ import luigi
 import os
 
 
+class StoreFile(luigi.ExternalTask):
+    input_filename = luigi.PathParameter()
+
+    def output(self):
+        return luigi.LocalTarget(self.input_filename)
+
+
 class StoreProductAnalysis(luigi.Task):
     """ This task analyzes the store product inventory and dynamics.
 
     """
     input_filename = luigi.PathParameter()
     output_directory = luigi.PathParameter(default="product_analysis_results")
+    store_name = luigi.Parameter()
 
     def requires(self):
         return
