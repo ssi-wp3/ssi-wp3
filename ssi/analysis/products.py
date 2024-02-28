e8662c1e9b5a648af8bc7008fd56ffc11c9fd45a
Tim De Jong <gitlab@thinkpractice.nl>
2024-02-28 09:56:08 +0100

    Added the log_texts_per_ean_histogram function.

--- ssi/analysis/products.py
+++ ssi/analysis/products.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def unique_texts_and_eans(dataframe: pd.DataFrame,
@@ -92,7 +93,7 @@ def unique_texts_and_eans_per_coicop(dataframe: pd.DataFrame,
 def texts_per_ean_histogram(dataframe: pd.DataFrame,
                             receipt_text_column: str = "receipt_text",
                             product_id_column: str = "ean_number"
-                            ) -> pd.DataFrame:
+                            ) -> pd.Series:
     """ This function calculates the histogram of the receipt texts per EAN. 
     First the number of unique texts per EAN is calculated. Then these counts 
     are binned a histogram of the counts is created. The histogram shows how
@@ -114,8 +115,8 @@ def texts_per_ean_histogram(dataframe: pd.DataFrame,
     Returns
     -------
 
-    pd.DataFrame
-        A dataframe containing the histogram of receipt text counts per of EAN.
+    pd.Series
+        A series containing the histogram of receipt text counts per of EAN.
     """
     texts_per_ean = dataframe.groupby(by=product_id_column)[
         receipt_text_column].nunique()
@@ -123,3 +124,39 @@ def texts_per_ean_histogram(dataframe: pd.DataFrame,
     receipt_text_counts = texts_per_ean.receipt_text.value_counts()
     receipt_text_counts = receipt_text_counts.sort_index()
     return receipt_text_counts
+
+
+def log_texts_per_ean_histogram(dataframe: pd.DataFrame,
+                                receipt_text_column: str = "receipt_text",
+                                product_id_column: str = "ean_number"
+                                ) -> pd.Series:
+    """ This function calculates the histogram of the receipt texts per EAN,
+    but instead of the function above returns the logarithm of the counts.
+
+    First the number of unique texts per EAN is calculated. Then these counts 
+    are binned a histogram of the counts is created. Then the logarithm is taken of
+    these counts. The histogram shows how often a certain number of texts per EAN occurs.
+
+    This can be used to create a histogram of the number of texts per EAN.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The input dataframe.
+
+    receipt_text_column : str
+        The column containing the receipt text. By default, it is "receipt_text".
+
+    product_id_column : str
+        The column containing the product ID. By default, it is "ean_number".
+
+    Returns
+    -------
+
+    pd.Series
+        A series containing the histogram of receipt text counts per of EAN.
+        The logarithm of the counts is returned instead of the count itself.
+    """
+    texts_per_ean_histogram = texts_per_ean_histogram(
+        dataframe, receipt_text_column, product_id_column)
+    return np.log(texts_per_ean_histogram)
