5b4c545a21cf8f5acc3460afec4d8414476cb70c
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-04 10:06:54 +0100

    Small fix.

--- luigi_combine_files.sh
+++ luigi_combine_files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PYTHONPATH="." luigi --module ssi.preprocessing.combine CombineAllRevenueFiles --input-directory=$1 -output-directory=$2 --local-scheduler 
+PYTHONPATH="." luigi --module ssi.preprocessing.combine CombineAllRevenueFiles --input-directory=$1 --output-directory=$2 --local-scheduler 
