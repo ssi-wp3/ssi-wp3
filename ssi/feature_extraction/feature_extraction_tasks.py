7d5a48e1201428342126a2098c75bc46e33be5a9
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-06 12:19:58 +0100

    More work on extracting features.

--- ssi/feature_extraction/feature_extraction_tasks.py
+++ ssi/feature_extraction/feature_extraction_tasks.py
@@ -1,12 +1,14 @@
 from .feature_extraction import FeatureExtractorFactory, FeatureExtractorType
 import luigi
+import pandas as pd
+import os
 
 
 class ParquetFile(luigi.ExternalTask):
     input_filename = luigi.Parameter()
 
     def output(self):
-        return luigi.LocalTarget(self.input_filename)
+        return luigi.LocalTarget(self.input_filename, format=luigi.format.Nop)
 
 
 class FeatureExtractionTask(luigi.Task):
@@ -15,10 +17,25 @@ class FeatureExtractionTask(luigi.Task):
     feature_extraction_method = luigi.EnumParameter(enum=FeatureExtractorType)
     batch_size = luigi.IntParameter(default=1000)
 
+    source_column = luigi.Parameter(default="receipt_text")
+    destination_column = luigi.Parameter(default="features")
+
     def requires(self):
         return ParquetFile(self.input_filename)
 
+    def output(self):
+        return luigi.LocalTarget(
+            os.path.join(self.output_directory, f"features_{self.feature_extraction_method}.parquet"), format=luigi.format.Nop)
+
     def run(self):
         feature_extractor_factory = FeatureExtractorFactory()
-        feature_extractor = feature_extractor_factory.create_feature_extractor(
-            self.feature_extraction_method)
+
+        with self.input().open('r') as input_file:
+            dataframe = pd.read_parquet(input_file)
+            feature_extractor_factory.extract_features_and_save(
+                dataframe,
+                self.source_column,
+                self.destination_column,
+                self.output().path,
+                batch_size=self.batch_size
+            )
