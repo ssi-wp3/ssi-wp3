3d02f81ebf43b5c58eaa817bbd00d5ee11726d86
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-06 13:16:28 +0100

    Added some TODOs to machine_learning_tasks.py.

---
+++ ssi/machine_learning/machine_learning_tasks.py
@@ -0,0 +1,5 @@
+
+# TODO add an evaluation that trains a model on one supermarket and evaluates it on another.
+# Check TFIDF and CountVectorizer for the feature extraction; they use a word dictionary,
+# this dictionary may be supermarket specific! i.e. features from one supermarket may not be usable with another.
+# TODO Return feature extraction pipeline instead?
