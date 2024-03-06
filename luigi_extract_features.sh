c21ed79b7b6f41edfa5e144a8ca053eb181ca48a
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-06 12:42:16 +0100

    Small fix.

--- luigi_extract_features.sh
+++ luigi_extract_features.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PYTHONPATH="." luigi --module ssi.feature_extraction.feature_extraction_tasks ExtractFeaturesForAllFiles --feature_extraction_method $1 --local-scheduler
+PYTHONPATH="." luigi --module ssi.feature_extraction.feature_extraction_tasks ExtractFeaturesForAllFiles --feature-extraction-method $1 --local-scheduler
