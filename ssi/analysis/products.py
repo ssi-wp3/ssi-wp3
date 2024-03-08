ddd45682e700c9faa1e339babf46b33010a838dc
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-08 09:29:03 +0100

    Convert series to dataframe.

--- ssi/analysis/products.py
+++ ssi/analysis/products.py
@@ -161,7 +161,7 @@ def log_texts_per_ean_histogram(dataframe: pd.DataFrame,
     """
     texts_per_ean_histogram = texts_per_ean_histogram(
         dataframe, receipt_text_column, product_id_column)
-    return np.log(texts_per_ean_histogram)
+    return np.log(texts_per_ean_histogram).to_frame().T
 
 
 def add_lagged_columns(grouped_texts_eans_per_month: pd.DataFrame,
