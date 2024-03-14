4b042b7cc76ad804fdd565515177c59d6dc78749
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-14 12:12:27 +0100

    Removed logging.

--- ssi/analysis/tasks.py
+++ ssi/analysis/tasks.py
@@ -387,7 +387,6 @@ class PlotResults(luigi.Task):
         }
 
     def output(self):
-        print(f"Input: {self.input()}")
         output_dict = dict()
         for function_name in self.input().keys():
             if function_name in self.plot_settings:
