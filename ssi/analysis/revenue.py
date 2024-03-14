802d5e07c6cf04fcc939b9b1f85bac3dbf8a4de4
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-14 09:52:41 +0100

    Added a function that can create a revenue dataframe that can be used to make a sunburst plot.

--- ssi/analysis/revenue.py
+++ ssi/analysis/revenue.py
@@ -56,7 +56,7 @@ def total_revenue_per_products(dataframe: pd.DataFrame,
     pd.DataFrame
         A dataframe containing the product id, total amount and total revenue.    
     """
-    return dataframe.groupby(product_id_column).apply(lambda x: total_revenue(x, amount_column, revenue_column))
+    return dataframe.groupby(product_id_column).apply(lambda x: total_revenue(x, amount_column, revenue_column)).reset_index()
 
 
 def total_revenue_per_coicop(dataframe: pd.DataFrame,
@@ -85,7 +85,7 @@ def total_revenue_per_coicop(dataframe: pd.DataFrame,
     pd.DataFrame
         A dataframe containing the coicop code, total amount and total revenue.    
     """
-    return dataframe.groupby(coicop_column).apply(lambda x: total_revenue(x, amount_column, revenue_column))
+    return dataframe.groupby(coicop_column).apply(lambda x: total_revenue(x, amount_column, revenue_column)).reset_index()
 
 
 def total_revenue_per_period(dataframe: pd.DataFrame,
@@ -114,7 +114,7 @@ def total_revenue_per_period(dataframe: pd.DataFrame,
     pd.DataFrame
         A dataframe containing the period, total amount and total revenue.    
     """
-    return dataframe.groupby(period_column).apply(lambda x: total_revenue(x, amount_column, revenue_column))
+    return dataframe.groupby(period_column).apply(lambda x: total_revenue(x, amount_column, revenue_column)).reset_index()
 
 
 def total_revenue_per_coicop_and_period(dataframe: pd.DataFrame,
@@ -147,7 +147,7 @@ def total_revenue_per_coicop_and_period(dataframe: pd.DataFrame,
     pd.DataFrame
         A dataframe containing the product id, period, total amount and total revenue.    
     """
-    return dataframe.groupby([period_column, coicop_column]).apply(lambda x: total_revenue(x, amount_column, revenue_column))
+    return dataframe.groupby([period_column, coicop_column]).apply(lambda x: total_revenue(x, amount_column, revenue_column)).reset_index()
 
 
 def revenue_for_coicop_hierarchy(dataframe: pd.DataFrame,
@@ -161,4 +161,25 @@ def revenue_for_coicop_hierarchy(dataframe: pd.DataFrame,
     ----------
     dataframe : pd.DataFrame
         The dataframe containing the revenue data.
+
+    coicop_columns : List[str]
+        The list of column names containing the subsequent coicop levels.
+
+    amount_column : str
+        The name of the column containing the amount of products.
+
+    revenue_column : str
+        The name of the column containing the revenue.
+
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe containing the total amount and total revenue for each subsequent coicop level.
+        For each coicop level analysed there's a column in the dataframe containing the coicop label.
+        The amount and revenue columns specify the values for each leaf in the coicop hierarchy.
+        This dataframe can be used to create sunburst plot of the product revenue.          
     """
+    return dataframe.groupby(coicop_columns).apply(lambda x: total_revenue(x, amount_column, revenue_column)).reset_index()
+
+
+# TODO revenue scatter plot
