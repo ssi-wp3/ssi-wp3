7e56701c279964900de3e4190d76e944251b1231
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-18 10:50:43 +0100

    Added project prefix and feature_file_suffix to files methods for feature files.

--- ssi/files.py
+++ ssi/files.py
@@ -2,11 +2,17 @@ from typing import List
 import os
 
 
-def get_feature_filename(feature_extractor_type: str, supermarket_name: str) -> str:
-    return f"ssi_{supermarket_name.lower()}_{feature_extractor_type}_features.parquet"
+def get_feature_filename(feature_extractor_type: str,
+                         supermarket_name: str,
+                         project_prefix: str = "ssi",
+                         feature_file_suffix: str = "features") -> str:
+    return f"{project_prefix}_{supermarket_name.lower()}_{feature_extractor_type}_{feature_file_suffix}.parquet"
 
 
-def get_features_files_in_directory(directory: str, extension: str = ".parquet") -> List[str]:
+def get_features_files_in_directory(directory: str,
+                                    project_prefix: str = "ssi",
+                                    feature_file_suffix: str = "features",
+                                    extension: str = ".parquet") -> List[str]:
     return [filename
             for filename in os.listdir(directory)
-            if filename.startswith("ssi_") and "_features" in filename and filename.endswith(extension)]
+            if filename.startswith(f"{project_prefix}_") and f"_{feature_file_suffix}" in filename and filename.endswith(extension)]
