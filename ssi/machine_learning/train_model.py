09f81e78d42f20e7ec2b788a43f578318a6bfff6
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-26 11:19:04 +0100

    Some cleanup.

--- ssi/machine_learning/train_model.py
+++ ssi/machine_learning/train_model.py
@@ -71,13 +71,6 @@ def evaluate(y_true: np.array, y_pred: np.array, suffix: str = "") -> Dict[str, 
     }
 
 
-def get_model(model_type: str, number_of_jobs: int = -1, verbose: bool = False):
-    model_factory = ModelFactory()
-    model = model_factory.create_model(
-        model_type)  # , n_jobs=number_of_jobs)
-    return model
-
-
 def fit_pipeline(train_dataframe: pd.DataFrame,
                  pipeline: Pipeline,
                  receipt_text_column: str,
@@ -95,7 +88,9 @@ def train_model(train_dataframe: pd.DataFrame,
                 label_column: str,
                 verbose: bool = False
                 ) -> Pipeline:
-    model = get_model(model_type, verbose=verbose)
+    model_factory = ModelFactory()
+    model = model_factory.create_model(
+        model_type)
     pipeline = Pipeline([
         ('classifier', model)
     ], verbose=verbose)
