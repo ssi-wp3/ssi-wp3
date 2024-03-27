123889794074c075486deabdf4430ae2fb55ca56
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-27 16:49:28 +0100

    Working on generic reporting classes.

--- ssi/analysis/report.py
+++ ssi/analysis/report.py
@@ -1,12 +1,54 @@
 from typing import Dict, Any, List
+from enum import Enum
 from .utils import unpivot
 from ..plots import PlotEngine
 from ..settings import Settings
 import pandas as pd
 
 
+class ReportEngine:
+    def __init__(self, settings: Settings):
+        self.__settings = settings
+
+    @property
+    def settings(self) -> Settings:
+        return self.__settings
+
+    @property
+    def output_filenames(self) -> Dict[str, str]:
+        return {result_key: result_settings["filename"]
+                for result_key, result_settings in self.settings.items()}
+
+
+class ReportType(Enum):
+    plot = "plot"
+    table = "table"
+
+
 class Report:
-    def __init__(self, plot_engine: PlotEngine):
+    def __init__(self, settings: Dict[str, Any]):
+        self.__settings = settings
+
+    @property
+    def settings(self) -> Dict[str, Any]:
+        return self.__settings
+
+    @property
+    def type(self) -> ReportType:
+        return ReportType[self.settings["type"].lower()]
+
+    @property
+    def type_settings(self) -> Dict[str, Any]:
+        return self.settings["settings"]
+
+    @property
+    def output_filename(self) -> str:
+        return self.settings["output_filename"]
+
+
+class PlotResult(Report):
+    def __init__(self, settings: Dict[str, Any], plot_engine: PlotEngine = PlotEngine()):
+        super().__init__(settings)
         self.__plot_engine = plot_engine
 
     @property
@@ -25,3 +67,11 @@ class Report:
         figure = self.plot_engine.plot_from_settings(
             dataframe, plot_settings["plot_settings"])
         figure.save(output_file)
+
+
+class TableResult(Report):
+    def __init__(self, settings: Dict[str, Any]):
+        super().__init__(settings)
+
+    def to_table(self, dataframe: pd.DataFrame, output_file: str):
+        dataframe.to_csv(output_file, index=False)
