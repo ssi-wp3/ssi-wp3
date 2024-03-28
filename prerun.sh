a47423f3c412eddac658da4d3c81b504ea82537a
Tim De Jong <gitlab@thinkpractice.nl>
2024-03-28 14:50:32 +0100

    Set standard configuration for git pull in prerun.sh.

--- prerun.sh
+++ prerun.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # This script is run by cnvrg to be able to start before an experiment or notebook
 export data_directory=/netappdata/ssi_tdjg/data/ssi
+
+# Configure git
+git config pull.rebase false
