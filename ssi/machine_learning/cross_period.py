0d1e5cb3e238830a32b3051c711acbdf0d13f5ea
Tim De Jong <gitlab@thinkpractice.nl>
2024-04-05 13:17:20 +0200

    Removed non-working fix.

--- ssi/machine_learning/cross_period.py
+++ ssi/machine_learning/cross_period.py
@@ -207,7 +207,7 @@ class TrainModelOnPeriod(TrainModelTask):
 
         def loss_function(y_pred, y):
             print(f"y_pred: {y_pred}, y: {y}")
-            return criterion(y_pred, y.reshape(-1))
+            return criterion(y_pred, y)
 
         train_engine = create_supervised_trainer(
             model,
