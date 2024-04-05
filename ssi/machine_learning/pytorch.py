5fb55c62d31482f7f19b303373ecaf38705f1f48
Tim De Jong <gitlab@thinkpractice.nl>
2024-04-05 16:13:17 +0200

    Log label tensor.

--- ssi/machine_learning/pytorch.py
+++ ssi/machine_learning/pytorch.py
@@ -253,6 +253,7 @@ class ParquetDataset(torch.utils.data.Dataset):
         one_hot_label = F.one_hot(label_tensor, num_classes=len(
             self.label_encoder.classes_)).float()
 
+        print("One Hot Label: ", one_hot_label)
         print("Target Shape:", one_hot_label.shape)
         return feature_tensor, one_hot_label
 
