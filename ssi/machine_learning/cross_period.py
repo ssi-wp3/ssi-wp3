96500e7428ff332490f5b2887f3e47f1e7ae7007
Tim De Jong <gitlab@thinkpractice.nl>
2024-04-10 11:25:26 +0200

    Log input and output dimensions for model.

--- ssi/machine_learning/cross_period.py
+++ ssi/machine_learning/cross_period.py
@@ -157,6 +157,8 @@ class TrainModelOnPeriod(TrainModelTask):
         # experiment = Experiment()
 
         print(f"Training model on {device} with learning rate {learning_rate}, num_epochs {num_epochs}, batch_size {batch_size}, number of epochs {num_epochs}, and early stopping patience {early_stopping_patience}")
+        print(
+            f"Model input dim: {self.feature_vector_size}, output dim: {self.number_of_categories}")
         model = TorchLogisticRegression(
             input_dim=self.feature_vector_size,
             output_dim=self.number_of_categories
