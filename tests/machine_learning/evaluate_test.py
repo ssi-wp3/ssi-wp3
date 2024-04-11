c09e25ce5087099b9c991c162eb27b977d2a5a19
Tim De Jong <gitlab@thinkpractice.nl>
2024-04-11 11:52:24 +0200

    Turned machine_learning test directory into a package to allow for package discrovery. Implemented first unit test for ConfusionMatrix class.

--- tests/machine_learning/evaluate_test.py
+++ tests/machine_learning/evaluate_test.py
@@ -1,7 +1,11 @@
-from ssi.machine_learning.evaluate import evaluate
+from ssi.machine_learning.evaluate import ConfusionMatrix
 import unittest
+import numpy as np
 
 
-class TestEvaluateFunctions(unittest.TestCase):
-    def test_evaluate(self):
-        self.assertEqual(evaluate(), "evaluate")
+class ConfusionMatrixTest(unittest.TestCase):
+    def test_confusion_matrix_label_mapping(self):
+        count_matrix = np.array([[1, 2], [3, 4]])
+        label_mapping = {"a": 0, "b": 1}
+        confusion_matrix = ConfusionMatrix(count_matrix, label_mapping)
+        self.assertEqual(label_mapping, confusion_matrix.label_mapping)
