0aa201eab1d2a0f8c15826d17719dbad33c4bd32
Tim De Jong <gitlab@thinkpractice.nl>
2024-04-16 15:39:20 +0200

    Added ConvertAllJumboReceipts to script.

--- luigi_convert_receipts.sh
+++ luigi_convert_receipts.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 PYTHONPATH="." luigi --module ssi.preprocessing.convert ConvertCSVToParquet --input-filename="${data_directory}/preprocessing/cleaned/receipts_plus_202201_Prd.csv" --output-directory="${data_directory}/preprocessing/02-parquet" --local-scheduler
 PYTHONPATH="." luigi --module ssi.preprocessing.convert ConvertAHReceipts --input-filename=$1 --local-scheduler 
+PYTHONPATH="." luigi --module ssi.preprocessing.convert ConvertAllJumboReceipts --input-directory=$2 --local-scheduler 
