6d277861becfbdc90bc1f1c8f7dc833f41a850cd
Tim De Jong <gitlab@thinkpractice.nl>
2024-04-17 13:49:17 +0200

    Only read product_id_columns.

--- ssi/analysis/tasks.py
+++ ssi/analysis/tasks.py
@@ -359,7 +359,7 @@ class CrossStoreAnalysis(luigi.Task):
                             output_file, engine=self.parquet_engine)
 
     def read_store_file(self, input_file, store_name_column: str, store_name: str) -> pd.DataFrame:
-        return self.__add_store_name_column(pd.read_parquet(input_file, engine=self.parquet_engine), store_name, store_name_column)
+        return self.__add_store_name_column(pd.read_parquet(input_file, engine=self.parquet_engine, columns=self.product_id_columns), store_name, store_name_column)
 
     def __add_store_name_column(self,
                                 store_dataframe: pd.DataFrame,
