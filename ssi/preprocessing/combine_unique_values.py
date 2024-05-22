b2690ad2bab94f3859663cb0c77ecca227c07328
Tim De Jong <gitlab@thinkpractice.nl>
2024-05-22 19:11:26 +0200

    Write equal sized batches to see if that fixes our parquet file.

--- ssi/preprocessing/combine_unique_values.py
+++ ssi/preprocessing/combine_unique_values.py
@@ -9,7 +9,8 @@ import tqdm
 def combine_unique_column_values(filenames: List[str],
                                  output_filename: str,
                                  key_columns: List[str],
-                                 parquet_engine: str = "pyarrow"
+                                 parquet_engine: str = "pyarrow",
+                                 batch_size: int = 1000
                                  ):
     """ Combine unique column values from multiple files into a single file.
     """
@@ -40,6 +41,7 @@ def combine_unique_column_values(filenames: List[str],
         with tqdm.tqdm(total=read_dataset.metadata.num_rows) as progress_bar:
             progress_bar.set_description(
                 f"Writing unique values from {filename}")
+            current_batch = None
             for batch in read_dataset.iter_batches():
                 batch_df = batch.to_pandas()
                 # Retrieve the row indices in the range of this batch
@@ -50,15 +52,20 @@ def combine_unique_column_values(filenames: List[str],
                 # Retrieve the rows in the range of this batch
                 batch_rows = batch_df.loc[batch_indices]
                 progress_bar.set_description(f"Wrote {len(batch_rows)} rows")
+                current_batch = pd.concat(
+                    [current_batch, batch_rows]) if current_batch is not None else batch_rows
 
                 batch_table = pa.Table.from_pandas(
-                    batch_rows)
+                    current_batch)
 
                 if number_of_rows_read == 0:
                     pq_writer = pq.ParquetWriter(
                         output_filename, batch_table.schema)
 
-                pq_writer.write_table(batch_table)
+                if len(current_batch) == batch_size:
+                    pq_writer.write_table(current_batch)
+                    current_batch = None
+
                 number_of_rows_read += len(batch)
                 progress_bar.update(len(batch))
             if pq_writer:
