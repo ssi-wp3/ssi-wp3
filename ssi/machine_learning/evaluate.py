15601f1b372488e56049433853b8d7878cdf337d
Tim De Jong <gitlab@thinkpractice.nl>
2024-06-04 09:20:56 +0200

    Added option to add labels to confusion matrix.

--- ssi/machine_learning/evaluate.py
+++ ssi/machine_learning/evaluate.py
@@ -230,9 +230,12 @@ def save_metrics(accuracy, precision, recall, f1, auc_roc, output_path):
             f'Accuracy: {accuracy}\nPrecision: {precision}\nRecall: {recall}\nF1-score: {f1}\nAUC-ROC: {auc_roc}')
 
 
-def plot_confusion_matrix(y_true, y_pred, output_path):
+def plot_confusion_matrix(y_true: np.array, y_pred: np.array, output_path: str, labels: List[str] = None):
     # Plot and save confusion matrix
     cm = confusion_matrix(y_true, y_pred)
+    if labels:
+        cm = pd.DataFrame(cm, index=labels, columns=labels)
+
     plt.figure(figsize=(10, 7))
     sns.heatmap(cm, annot=True, fmt='d')
     plt.title('Confusion Matrix')
