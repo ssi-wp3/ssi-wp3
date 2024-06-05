a70d0b4802b66eefbc4c106c787a3237f34b6e4f
Tim De Jong <gitlab@thinkpractice.nl>
2024-06-05 14:31:35 +0200

    Added product_availability_in_period to analyses.

--- ssi/analysis/tasks.py
+++ ssi/analysis/tasks.py
@@ -120,6 +120,7 @@ class PeriodAnalysis(BaseStoreAnalysisTask):
             "total_revenue_per_period": lambda dataframe: total_revenue_per_period(dataframe, self.period_column, self.amount_column, self.revenue_column),
             "unique_column_values_per_period": lambda dataframe: unique_column_values_per_period(dataframe, self.period_column, value_columns),
             "product_revenue_versus_lifetime": lambda dataframe: product_revenue_versus_lifetime(dataframe, self.period_column, self.product_id_column, self.amount_column, self.revenue_column),
+            "product_availability_in_period": lambda dataframe: product_availability_in_period(dataframe, self.product_id_column, self.period_column),
         }
 
     def get_store_analysis_filename(self, function_name: str) -> str:
