441bf58bc9a084e4b45e026a8b8eeebbd5e57fc9
Tim De Jong <gitlab@thinkpractice.nl>
2024-06-17 11:44:04 +0200

    We should convert all csvs to parquet instead.

--- luigi_convert_receipts.sh
+++ luigi_convert_receipts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-PYTHONPATH="." luigi --module ssi.preprocessing.convert ConvertCSVToParquet --local-scheduler 
+PYTHONPATH="." luigi --module ssi.preprocessing.convert ConvertAllCSVToParquet --local-scheduler 
 PYTHONPATH="." luigi --module ssi.preprocessing.convert ConvertAHReceipts --local-scheduler 
 PYTHONPATH="." luigi --module ssi.preprocessing.convert ConvertAllJumboReceipts --local-scheduler 
