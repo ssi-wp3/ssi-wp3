7a1a0077218e860383239861ca18d01e00d5e229
Tim De Jong <gitlab@thinkpractice.nl>
2024-06-19 10:02:35 +0200

    More work on DataProviderFactory.

--- ssi/machine_learning/data_loaders/data.py
+++ ssi/machine_learning/data_loaders/data.py
@@ -62,11 +62,12 @@ class DataProviderType(enum.Enum):
 
 class DataProviderFactory:
     @staticmethod
-    def create_data_provider(filename: str,
-                             data_provider_type: DataProviderType,
+    def create_data_provider(data_provider_type: DataProviderType,
+                             features_column: str,
+                             label_column: str,
                              **kwargs) -> 'DataProvider':
         if data_provider_type == DataProviderType.DataFrame:
-            # return DataframeDataProvider(features_column, label_column, label_encoder, **kwargs)
+            return DataframeDataProvider(features_column, label_column, **kwargs)
         else:
             raise ValueError(
                 f"Data provider type {data_provider_type} is not supported.")
