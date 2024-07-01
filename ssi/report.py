6755be5a17121747e49059128a223156cc12e782
Tim De Jong <gitlab@thinkpractice.nl>
2024-07-01 15:08:45 +0200

    Check if copying helps at all.

--- ssi/report.py
+++ ssi/report.py
@@ -510,8 +510,8 @@ class ReportEngine:
                 progress_bar.update(1)
 
     def preprocess_data(self, dataframe: pd.DataFrame, preprocessing_settings: Dict[str, Any]) -> pd.DataFrame:
-        if len(set(["pivot", "select_columns", "sort_values"]).intersection(set(preprocessing_settings.keys()))) > 0:
-            dataframe = dataframe.copy()
+        # if len(set(["pivot", "select_columns", "sort_values"]).intersection(set(preprocessing_settings.keys()))) > 0:
+        dataframe = dataframe.copy()
 
         if "pivot" in preprocessing_settings:
             value_columns = preprocessing_settings["value_columns"]
