aad3b77c653de64434b14f99d4cd985d5bf84a25
clam <c.lam@cbs.nl>
2024-07-16 15:56:30 +0000

    work on preprocessing

--- Analyses/0_preprocess.py
+++ Analyses/0_preprocess.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd 
 
 import config
@@ -13,6 +14,7 @@ import config
 LOAD_COLUMNS = [
  'ean_name',
  'product_id',
+ 'receipt_text', 
  'store_id',
  'store_name',
  'coicop_name',
@@ -26,10 +28,60 @@ LOAD_COLUMNS = [
  'month',
 ]
 
-if __name__ == "__main__":
-  for data_path in config.SOURCE_DATA_PATHS:
-    df = pd.read_parquet(data_path)
-    print(df.columns)
-    import pdb; pdb.set_trace()
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+  # drop 99999
+  df = df[df["coicop_number"] != "999999"]
 
+  df = df.sort_values("year_month", ascending=False)
+
+  # group by ean name, receipt text, and coicop
+  groupby_cols = ["ean_name", "receipt_text", "coicop_number"]
+
+  assert all(col in df.columns for col in groupby_cols)
+
+  df = df.set_index(groupby_cols)
+
+  df["count"] = df.groupby(groupby_cols).size()
+  df = df.reset_index()
+  df = df.drop_duplicates(subset=groupby_cols, keep="first")
+
+  df["count_weight_col_name"] = 1.0
+  import pdb; pdb.set_trace()
+  print(df.groupby("receipt_text")["count"] / df.groupby("receipt_text")["count"].sum())
+
+
+
+
+  #
+  # add weights
+  #
+  # weight_col_name
+  # date_weight_col_name
+  # count_weight_col_name
+
+  # add 
+
+  return df
+
+#def 
+
+if __name__ == "__main__":
+  df_stores = [] # all stores
+
+  for store_name in config.STORES:
+    dataset_fn = f"ssi_{store_name}_revenue.parquet"
+    dataset_path = os.path.join(config.SOURCE_DATA_DIR, dataset_fn)
+
+    df = pd.read_parquet(dataset_path, columns=LOAD_COLUMNS)
+    preprocess(df)
+    import pdb; pdb.set_trace()
+    df_stores.append(df)
+
+  df_stores = pd.concat(df_stores)
+
+  df_stores_preprocessed = preprocess(df_stores)
+
+  # 
+
+  # dfs[supermarket_name] = pd.read_parquet(data_file_path, columns=LOAD_COLUMNS)
 
--- Analyses/config.py
+++ Analyses/config.py
@@ -1,7 +1,6 @@
 import os
 
-SUPERMARKETS = ["lidl", "ah", "jumbo", "plus"]
+STORES = ["lidl", "ah", "jumbo", "plus"]
 
 SOURCE_DATA_DIR = os.path.join('/', "netappdata", "ssi_tdjg", "data", "ssi", "preprocessing", "05-final")
-SOURCE_DATA_PATHS = [os.path.join(SOURCE_DATA_DIR, f"ssi_{sm}_revenue.parquet") for sm in SUPERMARKETS]
 
