bb14cc6b969d44c3e491b3c21cdb66740b1eeb93
clam <c.lam@cbs.nl>
2024-07-22 10:33:18 +0000

    hierachical classifier working

--- Analyses/2_experiments.py
+++ Analyses/2_experiments.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, Self
+from typing_extensions import Self
 import pandas as pd
 from sklearn.pipeline import Pipeline
 
@@ -16,7 +16,7 @@ import config
 def eval_pipeline(pipe: Pipeline, X_test: pd.DataFrame, y_test: pd.Series, sample_weight=None) -> None:
   #pipe.fit(X_dev, y_dev, clf__sample_weight=sample_weight)
 
-  y_pred  = pipe.predict(X_test)
+  y_pred = pipe.predict(X_test)
   #y_proba = pipe.predict_proba(X_test)
 
   print(accuracy_score(y_test, y_pred))
@@ -24,17 +24,18 @@ def eval_pipeline(pipe: Pipeline, X_test: pd.DataFrame, y_test: pd.Series, sampl
   print(classification_report(y_test, y_pred))
 
 class HierarchicalClassifier():
-  def __init__(self, depth):
+  # add multi core
+  def __init__(self, depth, n_jobs=-1):
     self.depth = depth
     self.root_clf = None
     self.clf_dict = dict()
 
-  def fit(self, X: pd.DataFrame, y: pd.Series) -> Self:
+  def fit(self, X: pd.Series, y: pd.DataFrame) -> Self:
     y_root = y["coicop_level_1"]
 
     root_clf = Pipeline([
       ("hv", HashingVectorizer(input="content", binary=True, dtype=bool)),
-      ("clf", SGDClassifier(loss="log_loss", n_jobs=6)),
+      ("clf", SGDClassifier(loss="perceptron", n_jobs=6)),
     ])
     root_clf.fit(X, y_root)
 
@@ -42,47 +43,46 @@ class HierarchicalClassifier():
 
     for level in range(2, self.depth + 1):
       parent_level = level - 1
-      parent_labels = pd.concat([X, y], axis=1).groupby(f"coicop_level_{parent_level}")
+      parent_labels = y.groupby(f"coicop_level_{parent_level}")
 
-      for parent_label, parent_df in parent_labels:
-        y_level = y[f"coicop_level_{level}"]
+      for parent_label, predict_df in parent_labels:
+        y_level = predict_df[f"coicop_level_{level}"]
+        X_level = X.loc[predict_df.index]
 
         if y_level.nunique() < 2: continue
         
         clf = Pipeline([
           ("hv", HashingVectorizer(input="content", binary=True, dtype=bool)),
-          ("clf", SGDClassifier(loss="log_loss", n_jobs=6)),
+          ("clf", SGDClassifier(loss="perceptron", n_jobs=6)),
         ])
-        clf.fit(X, y_level)
+        clf.fit(X_level, y_level)
 
         self.clf_dict[parent_label] = clf
 
     return self
 
-  def predict(self, X):
-    root_y_pred = self.root_clf.predict(X)
-    root_y_pred = pd.Series(root_y_pred, name="coicop_label")
-
-    df = pd.concat([X, root_y_pred], axis=1)
+  def predict(self, X: pd.Series):
+    ret = self.root_clf.predict(X)
+    ret = pd.Series(ret, name="coicop_label", index=X.index)
 
     for coicop_level in range(2, self.depth + 1):
-      y_pred_by_coicop = df.groupby("coicop_label")
+      y_pred_by_coicop = pd.DataFrame(ret.copy()).groupby("coicop_label")
 
       for coicop_label, df_coicop in y_pred_by_coicop:
         clf = self.clf_dict.get(coicop_label)
 
         if clf is None: continue
 
-        X_coicop = df_coicop.drop("coicop_label", axis=1)
+        X_coicop = X[df_coicop.index]
 
-        df.loc[X_coicop.index, "coicop_label"] = clf.predict(X_coicop)
+        ret.loc[X_coicop.index] = clf.predict(X_coicop)
 
-    return df["coicop_label"]
+    return ret
 
   def predict_proba(self, X):
     pass
 
-def get_X_y(df: pd.DataFrame, predict_depth: int) -> tuple[pd.DataFrame, pd.Series]:
+def get_X_y(df: pd.DataFrame, predict_depth: int) -> tuple[pd.Series, pd.Series]:
   X_col = "receipt_text" # text column
   y_cols = [f"coicop_level_{predict_level}" for predict_level in range(1, predict_depth+1)]
 
@@ -102,7 +102,7 @@ if __name__ == "__main__":
   df_dev  = pd.read_parquet(df_dev_path)
   df_test = pd.read_parquet(df_test_path)
 
-  predict_coicop = 5
+  predict_coicop = 4
 
   X_dev, y_dev = get_X_y(df_dev, predict_coicop)
   X_test, y_test = get_X_y(df_test, predict_coicop)
@@ -110,7 +110,11 @@ if __name__ == "__main__":
   hc = HierarchicalClassifier(depth=predict_coicop)
   hc.fit(X_dev, y_dev)
 
-  y_pred = hc.predict(X_dev)
+#  hc = Pipeline([
+#      ("hv", HashingVectorizer(input="content", binary=True, dtype=bool)),
+#      ("clf", SGDClassifier(loss="perceptron", n_jobs=6)),
+#  ])
+#  hc.fit(X_dev, y_dev[f"coicop_level_{predict_coicop}"])
 
   eval_pipeline(hc, X_test, y_test[f"coicop_level_{predict_coicop}"])
 
