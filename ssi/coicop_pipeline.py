1aecfc90f43714db4219330223796b82d68e7604
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-12 16:50:31 +0200

    Some changes to save hf tokenizer too from training script.

--- ssi/coicop_pipeline.py
+++ ssi/coicop_pipeline.py
@@ -1,15 +1,37 @@
 from ssi.coicop_json_parser import CoicopInputFile, CoicopOutputFile,  create_coicop_output_file
 from typing import Any, Dict, List
+from enum import Enum
+from abc import ABC, abstractmethod
+from transformers import AutoModelForSequenceClassification
+from datasets import Dataset
 import joblib
 
 
-class CoicopPipeline:
-    def __init__(self, pipeline_path: str):
-        # load the model
-        self.model = joblib.load(pipeline_path)
+class CoicopPipelineType(Enum):
+    sklearn = 1
+    hugging_face = 2
 
-    def __call__(self, inputs: List[str]):
-        return self.predict_proba(inputs)
+
+class Pipeline(ABC):
+    @abstractmethod
+    def load(self, pipeline_path: str):
+        pass
+
+    @abstractmethod
+    def predict(self, inputs: List[str]) -> List[str]:
+        pass
+
+    @abstractmethod
+    def predict_proba(self, inputs: List[str]) -> List[Dict[str, Any]]:
+        pass
+
+
+class SklearnPipeline(Pipeline):
+    def __init__(self):
+        self.model = None
+
+    def load(self, pipeline_path: str):
+        self.model = joblib.load(pipeline_path)
 
     def predict(self, inputs: List[str]) -> List[str]:
         return self.model.predict(inputs)
@@ -25,10 +47,60 @@ class CoicopPipeline:
             prediction_labels.append(label_predictions)
         return prediction_labels
 
+
+class HuggingFacePipeline(Pipeline):
+    def __init__(self):
+        self.model = None
+
+    def load(self, pipeline_path: str):
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            pipeline_path)
+
+    def predict(self, inputs: List[str]) -> List[str]:
+        return self.model.predict(inputs)["label"]
+
+    def predict_proba(self, inputs: List[str]) -> List[Dict[str, Any]]:
+        predictions = self.model.predict_proba(inputs)
+        prediction_labels = []
+        for prediction in predictions:
+            label_predictions = dict()
+            for index, probability in enumerate(prediction):
+                label = self.model.classes_[index]
+                label_predictions[label] = probability
+            prediction_labels.append(label_predictions)
+        return prediction_labels
+
+
+class PipelineFactory:
+    @staticmethod
+    def pipeline_for(pipeline_type: CoicopPipelineType) -> Pipeline:
+        if pipeline_type == CoicopPipelineType.sklearn:
+            return SklearnPipeline()
+        elif pipeline_type == CoicopPipelineType.hugging_face:
+            return HuggingFacePipeline()
+        else:
+            raise ValueError("Pipeline type not supported")
+
+
+class CoicopPipeline:
+    def __init__(self, pipeline_path: str, pipeline_type: CoicopPipelineType):
+        # load the model
+        self.model = PipelineFactory.pipeline_for(
+            pipeline_type).load(pipeline_path)
+
+    def __call__(self, inputs: List[str]):
+        return self.model.predict_proba(inputs)
+
+    def predict(self, inputs: List[str]) -> List[str]:
+        return self.model.predict(inputs)
+
+    def predict_proba(self, inputs: List[str]) -> List[Dict[str, Any]]:
+        return self.model.predict_proba(inputs)
+
     def predict_receipt(self, receipt_input: CoicopInputFile) -> CoicopOutputFile:
         receipt_ids = [item.id for item in receipt_input.receipt.items]
         receipt_descriptions = [item.description
                                 for item in receipt_input.receipt.items]
         predicted_probabilities = self.predict_proba(receipt_descriptions)
-        
+
         return create_coicop_output_file(receipt_input, receipt_ids, predicted_probabilities)
