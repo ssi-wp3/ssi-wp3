08885bf8a674966db21c2a4c8d6d0a59bbfe5865
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-15 14:54:06 +0200

    Added some logging.

--- predict_coicop.py
+++ predict_coicop.py
@@ -8,6 +8,8 @@ import json
 def main(args):
     try:
         pipeline = CoicopPipeline(args.pipeline_path, args.pipeline_type)
+        print(
+            f"Loading pipeline from {args.pipeline_path} with type {args.pipeline_type}")
         coicop_input_file = load_input_file(args.input_data)
         coicop_output_file = pipeline.predict_receipt(coicop_input_file)
 
