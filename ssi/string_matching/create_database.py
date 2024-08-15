5badbc440ca4b7f2a47139b7eefa6da1fabd5bd2
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-15 10:45:03 +0200

    Fixed typo.

--- ssi/string_matching/create_database.py
+++ ssi/string_matching/create_database.py
@@ -23,7 +23,7 @@ con = duckdb.connect(args.output_filename)
 # """)
 con.sql(f"""drop table if exists {args.receipt_text_table};
             create sequence seq_row_id start 1;
-            create table {args.receipt_text_table} as select next_val('seq_row_id') as index,  *
+            create table {args.receipt_text_table} as select nextval('seq_row_id') as index,  *
             from read_parquet('{args.input_filename}');
         """)
 con.sql(
