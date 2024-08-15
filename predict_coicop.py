3dc43998505f79aee9d351e1075769487036adea
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-15 13:53:31 +0200

    Made it possible to load a specific pipeline type.

--- predict_coicop.py
+++ predict_coicop.py
@@ -1,12 +1,13 @@
 from ssi.coicop_json_parser import load_input_file
-from ssi.coicop_pipeline import CoicopPipeline
+from ssi.coicop_pipeline import CoicopPipeline, CoicopPipelineType
 import argparse
 import json
 # Adapted from https://huggingface.co/scikit-learn/sklearn-transformers/blob/main/pipeline.py
 
+
 def main(args):
     try:
-        pipeline = CoicopPipeline(args.pipeline_path)
+        pipeline = CoicopPipeline(args.pipeline_path, args.pipeline_type)
         coicop_input_file = load_input_file(args.input_data)
         coicop_output_file = pipeline.predict_receipt(coicop_input_file)
 
@@ -21,6 +22,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Predict COICOP')
     parser.add_argument("-pp", "--pipeline-path", type=str,
                         required=True, help="Path to pipeline")
+    parser.add_argument("-pt", "--pipeline-type", type=CoicopPipelineType, choices=list(CoicopPipelineType),
+                        default=CoicopPipelineType.hugging_face, help="Type of pipeline to use for prediction")
     parser.add_argument("-i", "--input_data", type=str,
                         required=True, help="Path to the input json file")
     parser.add_argument("-o", "--output_data", type=str,
