758787aeb203a1a0fa51995a8507b067bd1a33a8
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-15 11:03:05 +0200

    Solved more ambiguity.

--- ssi/string_matching/search.py
+++ ssi/string_matching/search.py
@@ -21,7 +21,7 @@ con = duckdb.connect(args.input_filename)
 
 print("-"*80)
 for query_string in args.receipt_texts:
-    print(con.sql(f"""select st.index, {args.receipt_text_column}, coicop_number, score from 
+    print(con.sql(f"""select st.index, st.{args.receipt_text_column}, rt.coicop_number, st.score from 
             (
             SELECT *, fts_main_{args.receipt_text_table}.match_bm25(
                 index,
