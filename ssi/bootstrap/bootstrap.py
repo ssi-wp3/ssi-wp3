f4f9cc2dfefcbb9a001f6d2a0832ac327b88f28f
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-15 14:40:31 +0200

    Added a function that can take a ratio for the sample size instead.

--- ssi/bootstrap/bootstrap.py
+++ ssi/bootstrap/bootstrap.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from sklearn.utils import resample
 import pandas as pd
 
@@ -6,7 +7,7 @@ def bootstrap_sample(dataframe: pd.DataFrame,
                      replace: bool = True,
                      n_samples: int = None,
                      random_state: int = 42,
-                     ) -> [pd.DataFrame, pd.DataFrame]:
+                     ) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Use sklearn.utils.resample to return a bootstrapped sample of the input dataframe.
 
     Parameters
@@ -25,7 +26,7 @@ def bootstrap_sample(dataframe: pd.DataFrame,
 
     Returns
     -------
-    pd.DataFrame, pd.DataFrame
+    Tuple[pd.DataFrame, pd.DataFrame]
         The dataframe with the bootstrapped sample and a dataframe with the out of bag sample.
 
     """
@@ -33,3 +34,35 @@ def bootstrap_sample(dataframe: pd.DataFrame,
         dataframe.index, replace=replace, n_samples=n_samples, random_state=random_state)
     out_of_bag_indices = dataframe.index.difference(sample_indices)
     return dataframe.loc[sample_indices], dataframe.loc[out_of_bag_indices]
+
+
+def bootstrap_sample_with_ratio(dataframe: pd.DataFrame,
+                                sample_ratio: float = 1.0,
+                                replace: bool = True,
+                                random_state: int = 42,
+                                ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Use sklearn.utils.resample to return a bootstrapped sample of the input dataframe.
+    Instead of specifying the number of samples, the sample_ratio is used in this function to determine the number of samples. This function calculates the number of samples for the sample_ratio and calls the bootstrap_sample function.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The input dataframe.
+
+    sample_ratio : float
+        The ratio of samples to draw. If 1.0, it will be the same as the size of the input dataframe.
+
+    replace : bool
+        Whether to sample with replacement.
+
+    random_state : int
+        The random seed.
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, pd.DataFrame]
+        The dataframe with the bootstrapped sample and a dataframe with the out of bag sample.
+
+    """
+    n_samples = int(len(dataframe) * sample_ratio)
+    return bootstrap_sample(dataframe, replace=replace, n_samples=n_samples, random_state=random_state)
