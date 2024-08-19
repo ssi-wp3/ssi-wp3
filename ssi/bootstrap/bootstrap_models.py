a295f7841174f47ce60c5e650df254b20cf62057
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-19 11:58:47 +0200

    Working on bootstrap with augmenting.

--- ssi/bootstrap/bootstrap_models.py
+++ ssi/bootstrap/bootstrap_models.py
@@ -1,4 +1,15 @@
-import nlpaug.augmenter.char as nac
 from .bootstrap import BootstrapSample, perform_bootstrap
+import nlpaug.augmenter.char as nac
+import pandas as pd
 
-# def ocr_preprocessing
+
+def ocr_preprocessing(bootstrap_sample: BootstrapSample, receipt_text_column: str, number_of_texts: int) -> BootstrapSample:
+    """ Perform OCR preprocessing augmentation.
+
+    """
+
+    def ocr_augment(dataframe: pd.DataFrame, receipt_text_column: str, number_of_texts: int) -> pd.DataFrame:
+        aug = nac.OcrAug()
+        return aug.augment(dataframe[receipt_text_column], n=number_of_texts)
+
+    return BootstrapSample
