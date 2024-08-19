a6b828287ca20dd1a4e27834fcff79edf43c9fc6
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-19 16:07:27 +0200

    Created a task used to Bootstrap a model.

---
+++ ssi/bootstrap/tasks.py
@@ -0,0 +1,46 @@
+from ..parquet_file import ParquetFile
+from ..hyper_params.sampler import create_sampler_for_pipeline
+from .bootstrap_models import bootstrap_model
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+import luigi
+import pandas as pd
+
+
+class BootstapModelTask(luigi.Task):
+    input_filename = luigi.PathParameter()
+    output_directory = luigi.PathParameter()
+    number_of_bootstraps = luigi.IntParameter(default=10)
+    number_of_samples_per_bootstrap = luigi.IntParameter(default=10)
+    feature_column = luigi.Parameter(default='features')
+    label_column = luigi.Parameter(default='coicop_number')
+    random_state = luigi.IntParameter(default=42)
+    engine = luigi.Parameter(default='pyarrow')
+
+    def requires(self):
+        return ParquetFile(self.input_filename)
+
+    def output(self):
+        return luigi.LocalTarget('bootstrap_model_results.csv')
+
+    def run(self):
+        sklearn_pipeline = Pipeline([
+            ('vectorizer', CountVectorizer()),
+            ('clf', LogisticRegression())
+        ])
+
+        with self.input().open('r') as input_file:
+            dataframe = pd.read_parquet(input_file, engine=self.engine)
+
+            param_sampler = create_sampler_for_pipeline(
+                'CountVectorizer', 'LogisticRegression', self.number_of_bootstraps, self.random_state)
+            bootstrap_df = bootstrap_model(sklearn_pipeline,
+                                           param_sampler,
+                                           dataframe,
+                                           self.number_of_bootstraps,
+                                           self.number_of_samples_per_bootstrap,
+                                           self.feature_column,
+                                           self.label_column,
+                                           self.random_state)
+            bootstrap_df.to_csv(self.output().path, index=False)
--- ssi/hyper_params/sampler.py
+++ ssi/hyper_params/sampler.py
@@ -2,7 +2,13 @@ from sklearn.model_selection import ParameterSampler
 from .hyper_params import ParamDistributions
 
 
-def create_samples(model_type: str, number_of_iterations: int, random_state: int = 42) -> ParameterSampler:
+def create_sampler_for_model(model_type: str, number_of_iterations: int, random_state: int = 42) -> ParameterSampler:
     param_distributions = ParamDistributions.distributions_for_model(
         model_type)
+    return ParameterSampler(param_distributions, n_iter=number_of_iterations, random_state=random_state)
+
+
+def create_sampler_for_pipeline(feature_extractor: str, model_type: str, number_of_iterations: int, random_state: int = 42) -> ParameterSampler:
+    param_distributions = ParamDistributions.distributions_for_pipeline(
+        feature_extractor, model_type)
     return ParameterSampler(param_distributions, n_iter=number_of_iterations, random_state=random_state)
