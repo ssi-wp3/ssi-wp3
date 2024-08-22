7d9fdbba6505712f1df9f7a0fa42faaef3805841
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-22 11:25:50 +0200

    Drop empty receipt texts in the bootstrap.

--- ssi/machine_learning/tasks.py
+++ ssi/machine_learning/tasks.py
@@ -1,6 +1,7 @@
 from ..parquet_file import ParquetFile
 from .hyper_params.sampler import create_sampler_for_pipeline
 from .bootstrap.bootstrap_models import bootstrap_model
+from ..preprocessing.combine_unique_values import drop_empty_receipts
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -14,6 +15,7 @@ class BootstrapModelTask(luigi.Task):
     number_of_bootstraps = luigi.IntParameter(default=10)
     number_of_samples_per_bootstrap = luigi.IntParameter(default=10)
     feature_column = luigi.Parameter(default='features')
+    receipt_text_column = luigi.Parameter(default='receipt_text')
     label_column = luigi.Parameter(default='coicop_number')
     random_state = luigi.IntParameter(default=42)
     engine = luigi.Parameter(default='pyarrow')
@@ -32,6 +34,8 @@ class BootstrapModelTask(luigi.Task):
 
         with self.input().open('r') as input_file:
             dataframe = pd.read_parquet(input_file, engine=self.engine)
+            dataframe = drop_empty_receipts(
+                dataframe, self.receipt_text_column)
 
             param_sampler = (value for value in create_sampler_for_pipeline(
                 'CountVectorizer', 'LogisticRegression', self.number_of_bootstraps, self.random_state)
