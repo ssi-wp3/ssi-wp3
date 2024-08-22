c00cd16a567f9bcf97c11af6c1f20852375f5e0d
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-22 09:50:36 +0200

    Implemented a script to create files without duplicates for each supermarket.

---
+++ ssi/preprocessing/drop_duplicates.py
@@ -0,0 +1,58 @@
+from ..parquet_file import ParquetFile
+from .combine_unique_values import drop_duplicates_and_empty_receipts
+from .files import get_store_name_from_combined_filename, get_combined_revenue_files_in_folder
+import pandas as pd
+import luigi
+import os
+
+
+class DropDuplicatesTask(luigi.Task):
+    input_filename = luigi.Parameter()
+    output_filename = luigi.Parameter()
+    key_columns = luigi.ListParameter(
+        default=['receipt_text', 'coicop_number'])
+    receipt_text_column = luigi.Parameter(default='receipt_text')
+    drop_empty_receipt_texts = luigi.BoolParameter(default=True)
+    engine = luigi.Parameter(default='pyarrow')
+
+    def requires(self):
+        return ParquetFile(self.input_filename)
+
+    def output(self):
+        return luigi.LocalTarget(self.output_filename, format=luigi.format.Nop)
+
+    def run(self):
+        with self.input().open('r') as input_file:
+            dataframe = pd.read_parquet(input_file, engine=self.engine)
+            dataframe = drop_duplicates_and_empty_receipts(dataframe,
+                                                           self.key_columns,
+                                                           self.receipt_text_column,
+                                                           self.drop_empty_receipt_texts)
+
+            with self.output().open('w') as output_file:
+                dataframe.to_parquet(output_file, engine=self.engine)
+
+
+class DropDuplicatesForAllFiles(luigi.WrapperTask):
+    input_directory = luigi.Parameter()
+    output_directory = luigi.Parameter()
+    filename_prefix = luigi.Parameter()
+    key_columns = luigi.ListParameter(
+        default=['receipt_text', 'coicop_number'])
+    receipt_text_column = luigi.Parameter(default='receipt_text')
+    drop_empty_receipt_texts = luigi.BoolParameter(default=True)
+    engine = luigi.Parameter(default='pyarrow')
+
+    def requires(self):
+        for input_filename in get_combined_revenue_files_in_folder(
+                self.input_directory, self.filename_prefix):
+            store_name = get_store_name_from_combined_filename(input_filename)
+            yield DropDuplicatesTask(
+                input_filename=input_filename,
+                output_filename=os.path.join(
+                    self.output_directory, f"{self.filename_prefix}_{store_name}_unique_values.parquet"),
+                key_columns=self.key_columns,
+                receipt_text_column=self.receipt_text_column,
+                drop_empty_receipt_texts=self.drop_empty_receipt_texts,
+                engine=self.engine
+            )
