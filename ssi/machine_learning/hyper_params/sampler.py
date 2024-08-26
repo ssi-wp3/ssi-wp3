420bce9501df5899fb6c112b6896b4e4561f7597
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-26 09:17:24 +0200

    Use FeatureExtractorType.

--- ssi/machine_learning/hyper_params/sampler.py
+++ ssi/machine_learning/hyper_params/sampler.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import ParameterSampler
-from .hyper_params import ParamDistributions
+from .hyper_params import ParamDistributions, FeatureExtractorType
 
 
 def create_sampler_for_model(model_type: str, number_of_iterations: int, random_state: int = 42) -> ParameterSampler:
@@ -8,7 +8,7 @@ def create_sampler_for_model(model_type: str, number_of_iterations: int, random_
     return ParameterSampler(param_distributions, n_iter=number_of_iterations, random_state=random_state)
 
 
-def create_sampler_for_pipeline(feature_extractor: str, model_type: str, number_of_iterations: int, random_state: int = 42) -> ParameterSampler:
+def create_sampler_for_pipeline(feature_extractor: FeatureExtractorType, model_type: str, number_of_iterations: int, random_state: int = 42) -> ParameterSampler:
     param_distributions = ParamDistributions.distributions_for_pipeline(
         feature_extractor, model_type)
     return ParameterSampler(param_distributions, n_iter=number_of_iterations, random_state=random_state)
