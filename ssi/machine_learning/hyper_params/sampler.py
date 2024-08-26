49ec395db3dcb17a89aeb44fa6b9d770aace814b
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-26 10:04:13 +0200

    Refactoring for ModelType.

--- ssi/machine_learning/hyper_params/sampler.py
+++ ssi/machine_learning/hyper_params/sampler.py
@@ -1,14 +1,19 @@
 from sklearn.model_selection import ParameterSampler
-from .hyper_params import ParamDistributions, FeatureExtractorType
+from .hyper_params import ParamDistributions, FeatureExtractorType, ModelType
 
 
-def create_sampler_for_model(model_type: str, number_of_iterations: int, random_state: int = 42) -> ParameterSampler:
+def create_sampler_for_model(model_type: ModelType,
+                             number_of_iterations: int,
+                             random_state: int = 42) -> ParameterSampler:
     param_distributions = ParamDistributions.distributions_for_model(
         model_type)
     return ParameterSampler(param_distributions, n_iter=number_of_iterations, random_state=random_state)
 
 
-def create_sampler_for_pipeline(feature_extractor: FeatureExtractorType, model_type: str, number_of_iterations: int, random_state: int = 42) -> ParameterSampler:
+def create_sampler_for_pipeline(feature_extractor: FeatureExtractorType,
+                                model_type: ModelType,
+                                number_of_iterations: int,
+                                random_state: int = 42) -> ParameterSampler:
     param_distributions = ParamDistributions.distributions_for_pipeline(
         feature_extractor, model_type)
     return ParameterSampler(param_distributions, n_iter=number_of_iterations, random_state=random_state)
