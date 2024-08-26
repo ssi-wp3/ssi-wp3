50cddf683c7cfad2197545aef8d74a6b34248bfb
Tim De Jong <gitlab@thinkpractice.nl>
2024-08-26 09:20:49 +0200

    Make it possible to use different types of feature extractors.

--- ssi/machine_learning/tasks.py
+++ ssi/machine_learning/tasks.py
@@ -1,9 +1,9 @@
 from ..parquet_file import ParquetFile
-from .hyper_params.sampler import create_sampler_for_pipeline
+from .hyper_params.sampler import create_sampler_for_pipeline, FeatureExtractorType
 from .bootstrap.bootstrap_models import bootstrap_model
 from ..preprocessing.combine_unique_values import drop_empty_receipts
 from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
 from sklearn.linear_model import LogisticRegression
 import luigi
 import pandas as pd
@@ -14,6 +14,7 @@ class BootstrapModelTask(luigi.Task):
     output_filename = luigi.PathParameter()
     number_of_bootstraps = luigi.IntParameter(default=10)
     number_of_samples_per_bootstrap = luigi.IntParameter(default=10)
+    feature_extractor_type = luigi.EnumParameter(enum=FeatureExtractorType)
     feature_column = luigi.Parameter(default='features')
     receipt_text_column = luigi.Parameter(default='receipt_text')
     label_column = luigi.Parameter(default='coicop_number')
@@ -27,9 +28,18 @@ class BootstrapModelTask(luigi.Task):
     def output(self):
         return luigi.LocalTarget(self.output_filename, format=luigi.format.Nop)
 
+    def feature_extractor_for_type(self, feature_extractor_type: FeatureExtractorType):
+        if feature_extractor_type == FeatureExtractorType.count_vectorizer:
+            return CountVectorizer()
+        elif feature_extractor_type == FeatureExtractorType.tfidf_vectorizer:
+            return TfidfVectorizer()
+        elif feature_extractor_type == FeatureExtractorType.hashing_vectorizer:
+            return HashingVectorizer()
+
     def run(self):
         sklearn_pipeline = Pipeline([
-            ('vectorizer', CountVectorizer()),
+            ('vectorizer', self.feature_extractor_for_type(
+                self.feature_extractor_type)),
             ('clf', LogisticRegression())
         ])
 
@@ -39,7 +49,7 @@ class BootstrapModelTask(luigi.Task):
                 dataframe, self.receipt_text_column)
 
             param_sampler = (value for value in create_sampler_for_pipeline(
-                'CountVectorizer', 'LogisticRegression', self.number_of_bootstraps, self.random_state)
+                self.feature_extractor_type, 'LogisticRegression', self.number_of_bootstraps, self.random_state)
             )
 
             with self.output().open('w') as output_file:
