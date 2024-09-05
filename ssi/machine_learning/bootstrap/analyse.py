de37a99d333d68439e9df459867d898b3c85e396
Tim De Jong <gitlab@thinkpractice.nl>
2024-09-05 12:03:49 +0200

    Retrieve all files in directory instead.

--- ssi/machine_learning/bootstrap/analyse.py
+++ ssi/machine_learning/bootstrap/analyse.py
@@ -1,10 +1,11 @@
 from .results import boxplot_files
 import argparse
+import os
 
 parser = argparse.ArgumentParser(
     description='Create a boxplot for the given metrics.')
-parser.add_argument('-f', '--files', metavar='files', type=str, nargs='+',
-                    help='List of file names.')
+parser.add_argument('-i', '--input-directory',
+                    type=str, help='Input directory.')
 parser.add_argument('-d', '--delimiter', type=str, default=';',
                     help='Delimiter to use for reading the files.')
 parser.add_argument('-p', '--plot_filename', type=str, default='boxplot.png',
@@ -15,5 +16,7 @@ parser.add_argument('-g', '--group_name', type=str, default='store',
                     help='Name of the column containing the group names.')
 args = parser.parse_args()
 
-boxplot_files(args.files, args.delimiter, args.plot_filename,
+files = [os.path.join(args.input_directory, file)
+         for file in os.listdir(args.input_directory)]
+boxplot_files(files, args.delimiter, args.plot_filename,
               args.metric_name, args.group_name)
