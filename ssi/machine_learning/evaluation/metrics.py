c88938895eae12a6b6e272fbd3425f7bc0cd03ec
Tim De Jong <gitlab@thinkpractice.nl>
2024-10-01 10:24:56 +0200

    Added documentation to the metrics package.

--- ssi/machine_learning/evaluation/metrics.py
+++ ssi/machine_learning/evaluation/metrics.py
@@ -4,6 +4,20 @@ import pandas as pd
 
 
 def _get_coicop_level_label(y: pd.Series, level: int) -> np.ndarray:
+    """ Get the upper level labels for the hierarchical classification.
+
+    Parameters
+    ----------
+    y : pd.Series
+        The labels.
+    level : int
+        The level of the labels.
+
+    Returns
+    -------
+    np.ndarray
+        The upper level labels.
+    """
     if not isinstance(y, pd.Series):
         if isinstance(y, np.ndarray):
             y = pd.Series(y)
@@ -26,6 +40,26 @@ def _get_coicop_level_label(y: pd.Series, level: int) -> np.ndarray:
 
 
 def hierarchical_precision_score(y_true: np.ndarray, y_pred: np.ndarray, get_upper_level_func: callable = _get_coicop_level_label, predict_level: int = 5, average="binary") -> float:
+    """ Calculate the precision score for the hierarchical classification.
+
+    Parameters
+    ----------
+    y_true : np.ndarray
+        The true labels.
+    y_pred : np.ndarray
+        The predicted labels.
+    get_upper_level_func : callable
+        The function to get the upper level labels.
+    predict_level : int, optional
+        The level of the prediction, by default 5.
+    average : str, optional
+        The average method to use, by default "binary".
+
+    Returns
+    -------
+    float
+        The precision score for the hierarchical classification.
+    """
     y_true_all_levels = _concat_upper_levels(
         y_true, get_upper_level_func, predict_level)
     y_pred_all_levels = _concat_upper_levels(
@@ -35,6 +69,26 @@ def hierarchical_precision_score(y_true: np.ndarray, y_pred: np.ndarray, get_upp
 
 
 def hierarchical_recall_score(y_true: np.ndarray, y_pred: np.ndarray, get_upper_level_func: callable = _get_coicop_level_label, predict_level: int = 5, average="binary") -> float:
+    """ Calculate the recall score for the hierarchical classification.
+
+    Parameters
+    ----------
+    y_true : np.ndarray
+        The true labels.
+    y_pred : np.ndarray
+        The predicted labels.
+    get_upper_level_func : callable
+        The function to get the upper level labels.
+    predict_level : int, optional
+        The level of the prediction, by default 5.
+    average : str, optional
+        The average method to use, by default "binary".
+
+    Returns
+    -------
+    float
+        The recall score for the hierarchical classification.
+    """
     y_true_all_levels = _concat_upper_levels(
         y_true, get_upper_level_func, predict_level)
     y_pred_all_levels = _concat_upper_levels(
@@ -44,6 +98,26 @@ def hierarchical_recall_score(y_true: np.ndarray, y_pred: np.ndarray, get_upper_
 
 
 def hierarchical_f1_score(y_true: np.ndarray, y_pred: np.ndarray, get_upper_level_func: callable = _get_coicop_level_label, predict_level: int = 5, average="binary") -> float:
+    """ Calculate the F1 score for the hierarchical classification.
+
+    Parameters
+    ----------
+    y_true : np.ndarray
+        The true labels.
+    y_pred : np.ndarray
+        The predicted labels.
+    get_upper_level_func : callable
+        The function to get the upper level labels.
+    predict_level : int, optional
+        The level of the prediction, by default 5.
+    average : str, optional
+        The average method to use, by default "binary".
+
+    Returns
+    -------
+    float
+        The F1 score for the hierarchical classification.
+    """
     y_true_all_levels = _concat_upper_levels(
         y_true, get_upper_level_func, predict_level)
     y_pred_all_levels = _concat_upper_levels(
@@ -53,6 +127,22 @@ def hierarchical_f1_score(y_true: np.ndarray, y_pred: np.ndarray, get_upper_leve
 
 
 def _concat_upper_levels(y: np.ndarray, get_upper_level_func: callable, predict_level: int) -> np.ndarray:
+    """ Concatenate the upper level labels for the hierarchical classification.
+
+    Parameters
+    ----------
+    y : np.ndarray
+        The labels.
+    get_upper_level_func : callable
+        The function to get the upper level labels.
+    predict_level : int
+        The level of the prediction.
+
+    Returns
+    -------
+    np.ndarray
+        The concatenated upper level labels.
+    """
     ret = [y]
 
     for level in range(1, predict_level):
