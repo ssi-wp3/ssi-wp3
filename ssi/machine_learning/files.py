3769af1e4018eccf517769da1c1e407c8c73be3f
Tim De Jong <gitlab@thinkpractice.nl>
2024-10-01 10:53:17 +0200

    Added documentation for files.py and hiclass_model.py.

--- ssi/machine_learning/files.py
+++ ssi/machine_learning/files.py
@@ -3,14 +3,37 @@ import os
 
 
 class MLDirectories:
+    """ Class to manage the directories for the machine learning pipeline. """
+
     def __init__(self, base_directory: str):
+        """ Initialize the directories.
+
+        Parameters
+        ----------
+        base_directory : str
+            The base directory
+        """
         self.__base_directory = base_directory
 
     @property
     def base_directory(self) -> str:
+        """ Get the base directory.
+
+        Returns
+        -------
+        str
+            The base directory
+        """
         return self.__base_directory
 
     @property
     def directories(self) -> List[str]:
+        """ Get the directories.
+
+        Returns
+        -------
+        List[str]
+            The directories
+        """
         return [
         ]
--- ssi/machine_learning/hiclass_model.py
+++ ssi/machine_learning/hiclass_model.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 
 class HiClassModel(Model):
+    """ Class to wrap the HiClass model. """
+
     def __init__(self,
                  model
                  ):
@@ -13,19 +15,73 @@ class HiClassModel(Model):
 
     @property
     def classifier(self):
+        """ Get the classifier.
+
+        Returns
+        -------
+        LocalClassifierPerParentNode
+            The classifier
+        """
         return self.__classifier
 
     def fit(self, X, y):
+        """ Fit the model to the data.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            The data
+        y : pd.Series
+            The labels
+        """
         self.classifier.fit(X, y)
         return self
 
     def predict(self, X):
+        """ Predict the labels for the data.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            The data
+
+        Returns
+        -------
+        np.array
+            The predicted labels
+        """
         return self.classifier.predict(X)
 
     def predict_proba(self, X):
+        """ Predict the probabilities for the data.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            The data
+
+        Returns
+        -------
+        np.array
+            The predicted probabilities
+        """
         return self.classifier.predict_proba(X)
 
     def score(self, X, y):
+        """ Score the model on the data.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            The data
+        y : pd.Series
+            The labels
+
+        Returns
+        -------
+        float
+            The score
+        """
         return self.classifier.score(X, y)
 
     def load_data(self, filename: str, **kwargs) -> pd.DataFrame:
